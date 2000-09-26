d27969c8abc487cb00ac21609c6815f5d0698f9d
Jeremy Siek <jeremy.siek@gmail.com>
2000-09-26 09:08:40 +0000

    doc edits for transpose graph and a few others
    [SVN r7844]

--- include/boost/graph/transpose_graph.hpp
+++ include/boost/graph/transpose_graph.hpp
@@ -33,63 +33,30 @@
 
 namespace boost {
 
-  // Transpose Graph
-  //
-  // To get the transpose of a directed graph. The transpose of a
-  // directed graph <tt>G = (V, E)</tt> is the graph <tt>GT = (V,
-  // ET)</tt>, where <tt>ET = {(v, u) in V x V: (u, v) in
-  // E}</tt>. i.e., <tt>GT</tt> is <tt>G</tt> with all its edges
-  // reversed.
-
-  // Graph G2 must have the same number of vertices as G1,
-  // and no edges. The edges will be added by calling
-  // <tt>G2.add_edge(u,v)</tt>.
-
-  // This version of <tt>transpose</tt> assumes the vertices
-  // in the graph model VertexWithID. The other version
-  // of transpose does not, are requests an ID Decorator
-  // function from the user.
-  //
-  template <class Graph1, class Graph2> 
-  void transpose_graph(const Graph1& G1, Graph2& G2)
+  template <class VertexListGraph, class MutableGraph> 
+  void transpose_graph(const VertexListGraph& G, MutableGraph& G_T)
   {
-    transpose_graph(G1, G2, get(vertex_index, G1));
+    transpose_graph(G, G_T, get(vertex_index, G));
   }
   
-
-  // Transpose  (with ID decorator function)
-  //
-  // To get the transpose of a directed graph. The transpose of a
-  // directed graph <tt>G = (V, E)</tt> is the graph <tt>GT = (V,
-  // ET)</tt>, where <tt>ET = {(v, u) in V x V: (u, v) in
-  // E}</tt>. i.e., <tt>GT</tt> is <tt>G</tt> with all its edges
-  // reversed.
-  // <p>
-  // Graph G2 must have the same number of vertices as G1,
-  // and no edges. The edges will be added by calling
-  // <tt>add_edge(G2, u,v)</tt>.
-  // <p>
-  // This version of <tt>transpose</tt> does not assume the vertices
-  // in the graph model VertexWithID, but instead requests an ID
-  // Decorator function from the user.  The other version of transpose
-  // assumes VertexWithID.
-  template <class Graph1, class Graph2, class ID> 
-  void transpose_graph(const Graph1& G1, Graph2& G2, ID id )
+  template <class VertexListGraph, class MutableGraph, class VertexIndexMap> 
+  void transpose_graph(const VertexListGraph& G, MutableGraph& G_T,
+                       VertexIndexMap index )
   {
-    typedef typename graph_traits<Graph1>::vertex_iterator VerIter;
-    typedef typename graph_traits<Graph1>::
+    typedef typename graph_traits<VertexListGraph>::vertex_iterator VerIter;
+    typedef typename graph_traits<VertexListGraph>::
       out_edge_iterator OutEdgeIter;
-    typedef typename graph_traits<Graph1>::vertex_descriptor Vertex;
-    typedef typename graph_traits<Graph1>::edge_descriptor Edge;
+    typedef typename graph_traits<VertexListGraph>::vertex_descriptor Vertex;
+    typedef typename graph_traits<VertexListGraph>::edge_descriptor Edge;
     
     VerIter i, iend;
     OutEdgeIter j, jend;
-    for ( boost::tie(i, iend) = vertices(G1); i!=iend; ++i) {
+    for ( boost::tie(i, iend) = vertices(G); i!=iend; ++i) {
       Vertex u = *i;
-      for (boost::tie(j,jend) = out_edges(u, G1); j !=jend; ++j) {
+      for (boost::tie(j,jend) = out_edges(u, G); j !=jend; ++j) {
         Edge e = *j;
-        Vertex v = target(e, G1);
-        add_edge(G2, id[v], id[u]);
+        Vertex v = target(e, G);
+        add_edge(G_T, vertex(get(index,v), G_T), vertex(get(index,u), G_T));
         // hmmm, what to do about edge properties?  should have an
         // add-edge(g,e) function, and have a templated edge
         // constructor -JGS
