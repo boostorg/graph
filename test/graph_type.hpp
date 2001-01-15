c093f455456483868011c31cfc762bd01fb7659b
Jeremy Siek <jeremy.siek@gmail.com>
2001-01-15 03:39:30 +0000

    changed slistS to listS to make it a portable test
    [SVN r8602]

--- test/graph_type.hpp
+++ test/graph_type.hpp
@@ -1,4 +1,4 @@
 #include <boost/graph/adjacency_list.hpp>
-typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, boost::property<vertex_id_t, std::size_t>, boost::property<edge_id_t, std::size_t> > Graph;
-typedef boost::property<vertex_id_t, std::size_t> VertexId;
+typedef boost::adjacency_list<boost::listS, boost::listS, boost::undirectedS, boost::property<vertex_id_t, std::size_t>, boost::property<edge_id_t, std::size_t> > Graph;
+typedef boost::property<vertex_id_t, std::size_t> VertexID;
 typedef boost::property<edge_id_t, std::size_t> EdgeID;
