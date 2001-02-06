cfa4f5c4a15bef6b82443392f0006eae2c97305b
Jeremy Siek <jeremy.siek@gmail.com>
2001-02-06 02:02:13 +0000

    removed #include
    [SVN r8965]

--- test/dfs.cpp
+++ test/dfs.cpp
@@ -6,7 +6,6 @@
 #include <boost/graph/depth_first_search.hpp>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graph_archetypes.hpp>
-#include "generate_random_graph.hpp"
 
 template <typename ColorMap, typename ParentMap,
   typename DiscoverTimeMap, typename FinishTimeMap>
