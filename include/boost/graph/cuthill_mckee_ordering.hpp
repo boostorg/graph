1df5e723948707c17d69ba6f9aeb248eab5dd50a
Jeremy Siek <jeremy.siek@gmail.com>
2001-02-26 23:17:15 +0000

    more edits
    [SVN r9345]

--- include/boost/graph/cuthill_mckee_ordering.hpp
+++ include/boost/graph/cuthill_mckee_ordering.hpp
@@ -36,7 +36,6 @@
 #include <boost/graph/properties.hpp>
 #include <boost/pending/indirect_cmp.hpp>
 #include <boost/property_map.hpp>
-#include <boost/detail/numeric_traits.hpp>
 
 /*
   (Reverse) Cuthill-McKee Algorithm for matrix reordering
@@ -236,68 +235,7 @@ namespace boost {
     //if G has several forests, how to let is cover all. ??
   }
 
-  template < class Graph, class OutputIterator, class Color >
-  inline void 
-  cuthill_mckee_ordering(Graph& G, OutputIterator inverse_permutation, 
-                         Color color)
-  {
-    typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
-    cuthill_mckee_ordering(G, inverse_permutation, color, 
-                           get(vertex_degree, G));
-  }
-  
-  template <class Graph, class OutputIterator>
-  inline void 
-  cuthill_mckee_ordering(Graph& G, OutputIterator inverse_permutation)
-  {
-    typedef typename boost::graph_traits<Graph>::vertex_descriptor Vertex;
-    cuthill_mckee_ordering(G, inverse_permutation, get(vertex_color, G));
-  }
-
-  template <typename Graph, typename VertexIndexMap>
-  typename graph_traits<Graph>::vertices_size_type
-  ith_bandwidth(typename graph_traits<Graph>::vertex_descriptor i,
-		const Graph& g,
-		VertexIndexMap index)
-  {
-    typedef typename graph_traits<Graph>::vertices_size_type size_type;
-    typedef typename detail::numeric_traits<size_type>::difference_type d_type;
-    d_type b = 0;
-    typename graph_traits<Graph>::out_edge_iterator e, end;
-    for (tie(e, end) = out_edges(i, g); e != end; ++e) {
-      b = std::max(b, std::abs(d_type(get(index, i)) - 
-			       d_type(get(index, target(*e, g)))));
-    }
-    return b;
-  }
-
-  template <typename Graph>
-  typename graph_traits<Graph>::vertices_size_type
-  ith_bandwidth(typename graph_traits<Graph>::vertex_descriptor i,
-		const Graph& g)
-  {
-    return ith_bandwidth(i, g, get(vertex_index, g));
-  }
-
-  template <typename Graph, typename VertexIndexMap>
-  typename graph_traits<Graph>::vertices_size_type
-  bandwidth(const Graph& g, VertexIndexMap index)
-  {
-    typename graph_traits<Graph>::vertices_size_type b = 0;
-    typename graph_traits<Graph>::vertex_iterator i, end;
-    for (tie(i, end) = vertices(g); i != end; ++i)
-      b = std::max(b, ith_bandwidth(*i, g, index));
-    return b;
-  }
-
-  template <typename Graph>
-  typename graph_traits<Graph>::vertices_size_type
-  bandwidth(const Graph& g)
-  {
-    return bandwidth(g, get(vertex_index, g));
-  }
-
-} /*namespace matrix_ordering*/
+} // namespace boost
 
 
 #endif // BOOST_GRAPH_CUTHILL_MCKEE_HPP
