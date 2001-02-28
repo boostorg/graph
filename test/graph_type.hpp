646965f4ca0d9dc630dd7d8bf938892e6604394a
Jeremy Siek <jeremy.siek@gmail.com>
2001-02-28 04:20:20 +0000

    few edits
    [SVN r9361]

---
+++ test/graph_type.hpp
@@ -0,0 +1,4 @@
+#include <boost/graph/adjacency_list.hpp>
+typedef boost::adjacency_list<boost::slistS, boost::slistS, boost::undirectedS, boost::property<vertex_id_t, std::size_t>, boost::property<edge_id_t, std::size_t> > Graph;
+typedef boost::property<vertex_id_t, std::size_t> VertexId;
+typedef boost::property<edge_id_t, std::size_t> EdgeID;
