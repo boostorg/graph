a99bad2398c5680707d0e071727d66205e738d60
Jeremy Siek <jeremy.siek@gmail.com>
2001-03-22 18:05:03 +0000

    removed self loop (1,1)
    [SVN r9632]

--- example/kruskal.cpp
+++ example/kruskal.cpp
@@ -55,12 +55,12 @@ int main(int , char* [])
   typedef std::pair<int,int> E;
   const int num_nodes = 5;
   E edges[] = { E(0,2), 
-                E(1,1), E(1,3), E(1,4),
+                E(1,3), E(1,4),
                 E(2,1), E(2,3), 
                 E(3,4),
                 E(4,0), E(4,1) };
   int weights[] = { 1, 
-                    2, 1, 2,
+                    1, 2,
                     7, 3, 
                     1,
                     1, 1};
