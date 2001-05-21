7762b3049e07a87b5f99424875545aed9d9db63f
Jeremy Siek <jeremy.siek@gmail.com>
2001-05-21 19:17:30 +0000

    added checks for const Graph*
    [SVN r10175]

--- test/stanford_graph_cc.cpp
+++ test/stanford_graph_cc.cpp
@@ -35,9 +35,27 @@ int main(int,char*[])
     typedef graph_traits<Graph>::vertex_descriptor Vertex;
     typedef graph_traits<Graph>::edge_descriptor Edge;
     function_requires< VertexListGraphConcept<Graph> >();
+    function_requires< IncidenceGraphConcept<Graph> >();
+    function_requires< AdjacencyGraphConcept<Graph> >();
     function_requires< PropertyGraphConcept<Graph, Edge, edge_length_t > >();
-    function_requires< PropertyGraphConcept<Graph, Vertex, u_property<Vertex> > >();
-    function_requires< PropertyGraphConcept<Graph, Edge, a_property<Vertex> > >();
+    function_requires< 
+      PropertyGraphConcept<Graph, Vertex, u_property<Vertex> > >();
+    function_requires< 
+      PropertyGraphConcept<Graph, Edge, a_property<Vertex> > >();
+  }
+  {
+    typedef const Graph* Graph;
+    typedef graph_traits<Graph>::vertex_descriptor Vertex;
+    typedef graph_traits<Graph>::edge_descriptor Edge;
+    function_requires< VertexListGraphConcept<Graph> >();
+    function_requires< IncidenceGraphConcept<Graph> >();
+    function_requires< AdjacencyGraphConcept<Graph> >();
+    function_requires< 
+      ReadablePropertyGraphConcept<Graph, Edge, edge_length_t > >();
+    function_requires< 
+      ReadablePropertyGraphConcept<Graph, Vertex, u_property<Vertex> > >();
+    function_requires< 
+      ReadablePropertyGraphConcept<Graph, Edge, a_property<Vertex> > >();
   }
   return 0;
 }
