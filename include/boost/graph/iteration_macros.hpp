571a855bbc4d70c62d0a503a016e1037737ae706
Jeremy Siek <jeremy.siek@gmail.com>
2001-12-05 03:40:10 +0000

    fixed a bug in some of them, can't assign to edge or vertex var in the initialization part, might be an empty sequence
    [SVN r11923]

--- include/boost/graph/iteration_macros.hpp
+++ include/boost/graph/iteration_macros.hpp
@@ -6,7 +6,7 @@
 #define BGL_LAST(linenum) BGL_CAT(bgl_last_,linenum)
 
 /*
-  BGL_FORALL_VERTICES(v, g, graph_t)  // This is on line 9
+  BGL_FORALL_VERTICES_T(v, g, graph_t)  // This is on line 9
   exapands to the following, but all on the same line
 
   typename boost::graph_traits<graph_t>::vertex_iterator 
@@ -17,10 +17,13 @@
        ++bgl_first_9)
 
   The other macros work in a similar fashion.
+
+  Use the _T versions when the graph type is a template parameter
+  or dependent on a template parameter.
  */
 
 
-#define BGL_FORALL_VERTICES(VNAME, GNAME, GraphType) \
+#define BGL_FORALL_VERTICES_T(VNAME, GNAME, GraphType) \
 typename boost::graph_traits<GraphType>::vertex_iterator BGL_FIRST(__LINE__),\
   BGL_LAST(__LINE__); \
 tie(BGL_FIRST(__LINE__), BGL_LAST(__LINE__)) = vertices(GNAME); \
@@ -29,39 +32,84 @@ for (typename boost::graph_traits<GraphType>::vertex_descriptor VNAME; \
   (VNAME = *BGL_FIRST(__LINE__), true) : false;\
    ++BGL_FIRST(__LINE__))
 
-#define BGL_FORALL_EDGES(ENAME, GNAME, GraphType) \
-typename boost::graph_traits<GraphType>::edge_iterator BGL_FIRST(__LINE__),\
+#define BGL_FORALL_VERTICES(VNAME, GNAME, GraphType) \
+boost::graph_traits<GraphType>::vertex_iterator BGL_FIRST(__LINE__),\
   BGL_LAST(__LINE__); \
-tie(BGL_FIRST(__LINE__), BGL_LAST(__LINE__)) = edge(GNAME); \
-for (typename boost::graph_traits<GraphType>::edge_descriptor ENAME \
-  = *BGL_FIRST(__LINE__); BGL_FIRST(__LINE__) != BGL_LAST(__LINE__) ? \
-  (ENAME = *BGL_FIRST(__LINE__), true) : false;\
-   ++BGL_FIRST(__LINE__))
-
-#define BGL_FORALL_ADJACENT(UNAME, VNAME, GNAME, GraphType) \
-typename boost::graph_traits<GraphType>::adjacency_iterator BGL_FIRST(__LINE__),\
-  BGL_LAST(__LINE__); \
-tie(BGL_FIRST(__LINE__), BGL_LAST(__LINE__)) = adjacent_vertices(UNAME, GNAME); \
-for (typename boost::graph_traits<GraphType>::vertex_descriptor VNAME \
-  = *BGL_FIRST(__LINE__); BGL_FIRST(__LINE__) != BGL_LAST(__LINE__) ? \
+tie(BGL_FIRST(__LINE__), BGL_LAST(__LINE__)) = vertices(GNAME); \
+for (boost::graph_traits<GraphType>::vertex_descriptor VNAME; \
+  BGL_FIRST(__LINE__) != BGL_LAST(__LINE__) ? \
   (VNAME = *BGL_FIRST(__LINE__), true) : false;\
    ++BGL_FIRST(__LINE__))
 
-#define BGL_FORALL_OUTEDGES(UNAME, ENAME, GNAME, GraphType) \
+#define BGL_FORALL_EDGES_T(ENAME, GNAME, GraphType) \
+typename boost::graph_traits<GraphType>::edge_iterator BGL_FIRST(__LINE__),\
+  BGL_LAST(__LINE__); \
+tie(BGL_FIRST(__LINE__), BGL_LAST(__LINE__)) = edge(GNAME); \
+for (typename boost::graph_traits<GraphType>::edge_descriptor ENAME; \
+  BGL_FIRST(__LINE__) != BGL_LAST(__LINE__) ? \
+  (ENAME = *BGL_FIRST(__LINE__), true) : false;\
+   ++BGL_FIRST(__LINE__))
+
+#define BGL_FORALL_EDGES(ENAME, GNAME, GraphType) \
+ boost::graph_traits<GraphType>::edge_iterator BGL_FIRST(__LINE__),\
+  BGL_LAST(__LINE__); \
+tie(BGL_FIRST(__LINE__), BGL_LAST(__LINE__)) = edge(GNAME); \
+for (boost::graph_traits<GraphType>::edge_descriptor ENAME; \
+  BGL_FIRST(__LINE__) != BGL_LAST(__LINE__) ? \
+  (ENAME = *BGL_FIRST(__LINE__), true) : false;\
+   ++BGL_FIRST(__LINE__))
+
+#define BGL_FORALL_ADJACENT_T(UNAME, VNAME, GNAME, GraphType) \
+typename boost::graph_traits<GraphType>::adjacency_iterator BGL_FIRST(__LINE__),\
+  BGL_LAST(__LINE__); \
+tie(BGL_FIRST(__LINE__), BGL_LAST(__LINE__)) = adjacent_vertices(UNAME, GNAME); \
+for (typename boost::graph_traits<GraphType>::vertex_descriptor VNAME; \
+  BGL_FIRST(__LINE__) != BGL_LAST(__LINE__) ? \
+  (VNAME = *BGL_FIRST(__LINE__), true) : false;\
+   ++BGL_FIRST(__LINE__))
+
+#define BGL_FORALL_ADJACENT(UNAME, VNAME, GNAME, GraphType) \
+ boost::graph_traits<GraphType>::adjacency_iterator BGL_FIRST(__LINE__),\
+  BGL_LAST(__LINE__); \
+tie(BGL_FIRST(__LINE__), BGL_LAST(__LINE__)) = adjacent_vertices(UNAME, GNAME); \
+for (boost::graph_traits<GraphType>::vertex_descriptor VNAME; \
+  BGL_FIRST(__LINE__) != BGL_LAST(__LINE__) ? \
+  (VNAME = *BGL_FIRST(__LINE__), true) : false;\
+   ++BGL_FIRST(__LINE__))
+
+#define BGL_FORALL_OUTEDGES_T(UNAME, ENAME, GNAME, GraphType) \
 typename boost::graph_traits<GraphType>::out_edge_iterator BGL_FIRST(__LINE__),\
   BGL_LAST(__LINE__); \
 tie(BGL_FIRST(__LINE__), BGL_LAST(__LINE__)) = out_edges(UNAME, GNAME); \
-for (typename boost::graph_traits<GraphType>::edge_descriptor ENAME \
-  = *BGL_FIRST(__LINE__); BGL_FIRST(__LINE__) != BGL_LAST(__LINE__) ? \
+for (typename boost::graph_traits<GraphType>::edge_descriptor ENAME; \
+  BGL_FIRST(__LINE__) != BGL_LAST(__LINE__) ? \
+  (ENAME = *BGL_FIRST(__LINE__), true) : false;\
+   ++BGL_FIRST(__LINE__))
+
+#define BGL_FORALL_OUTEDGES(UNAME, ENAME, GNAME, GraphType) \
+ boost::graph_traits<GraphType>::out_edge_iterator BGL_FIRST(__LINE__),\
+  BGL_LAST(__LINE__); \
+tie(BGL_FIRST(__LINE__), BGL_LAST(__LINE__)) = out_edges(UNAME, GNAME); \
+for (boost::graph_traits<GraphType>::edge_descriptor ENAME; \
+  BGL_FIRST(__LINE__) != BGL_LAST(__LINE__) ? \
+  (ENAME = *BGL_FIRST(__LINE__), true) : false;\
+   ++BGL_FIRST(__LINE__))
+
+#define BGL_FORALL_INEDGES_T(UNAME, ENAME, GNAME, GraphType) \
+typename boost::graph_traits<GraphType>::in_edge_iterator BGL_FIRST(__LINE__),\
+  BGL_LAST(__LINE__); \
+tie(BGL_FIRST(__LINE__), BGL_LAST(__LINE__)) = in_edges(UNAME, GNAME); \
+for (typename boost::graph_traits<GraphType>::edge_descriptor ENAME; \
+  BGL_FIRST(__LINE__) != BGL_LAST(__LINE__) ? \
   (ENAME = *BGL_FIRST(__LINE__), true) : false;\
    ++BGL_FIRST(__LINE__))
 
 #define BGL_FORALL_INEDGES(UNAME, ENAME, GNAME, GraphType) \
-typename boost::graph_traits<GraphType>::in_edge_iterator BGL_FIRST(__LINE__),\
+boost::graph_traits<GraphType>::in_edge_iterator BGL_FIRST(__LINE__),\
   BGL_LAST(__LINE__); \
 tie(BGL_FIRST(__LINE__), BGL_LAST(__LINE__)) = in_edges(UNAME, GNAME); \
-for (typename boost::graph_traits<GraphType>::edge_descriptor ENAME \
-  = *BGL_FIRST(__LINE__); BGL_FIRST(__LINE__) != BGL_LAST(__LINE__) ? \
+for (boost::graph_traits<GraphType>::edge_descriptor ENAME; \
+  BGL_FIRST(__LINE__) != BGL_LAST(__LINE__) ? \
   (ENAME = *BGL_FIRST(__LINE__), true) : false;\
    ++BGL_FIRST(__LINE__))
 
