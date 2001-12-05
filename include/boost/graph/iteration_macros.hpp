9177957cfc7c81bf31cc5d552767da0be043f8c9
Jeremy Siek <jeremy.siek@gmail.com>
2001-12-05 00:36:14 +0000

    added a comment to document how the macros work
    [SVN r11920]

--- include/boost/graph/iteration_macros.hpp
+++ include/boost/graph/iteration_macros.hpp
@@ -2,15 +2,30 @@
 #define BOOST_GRAPH_ITERATION_MACROS_HPP
 
 #define BGL_CAT(x,y) x ## y
-#define BGL_FIRST(line) BGL_CAT(first,line)
-#define BGL_LAST(line) BGL_CAT(last,line)
+#define BGL_FIRST(linenum) BGL_CAT(bgl_first_,linenum)
+#define BGL_LAST(linenum) BGL_CAT(bgl_last_,linenum)
+
+/*
+  BGL_FORALL_VERTICES(v, g, graph_t)  // This is on line 9
+  exapands to the following, but all on the same line
+
+  typename boost::graph_traits<graph_t>::vertex_iterator 
+     bgl_first_9, bgl_last_9;
+  tie(bgl_first_9, bgl_last_9) = vertices(g);
+  for (typename boost::graph_traits<graph_t>::vertex_descriptor v;
+       bgl_first_9 != bgl_last_9 ? (v = *bgl_first_9, true) : false;
+       ++bgl_first_9)
+
+  The other macros work in a similar fashion.
+ */
+
 
 #define BGL_FORALL_VERTICES(VNAME, GNAME, GraphType) \
 typename boost::graph_traits<GraphType>::vertex_iterator BGL_FIRST(__LINE__),\
   BGL_LAST(__LINE__); \
 tie(BGL_FIRST(__LINE__), BGL_LAST(__LINE__)) = vertices(GNAME); \
-for (typename boost::graph_traits<GraphType>::vertex_descriptor VNAME \
-  = *BGL_FIRST(__LINE__); BGL_FIRST(__LINE__) != BGL_LAST(__LINE__) ? \
+for (typename boost::graph_traits<GraphType>::vertex_descriptor VNAME; \
+  BGL_FIRST(__LINE__) != BGL_LAST(__LINE__) ? \
   (VNAME = *BGL_FIRST(__LINE__), true) : false;\
    ++BGL_FIRST(__LINE__))
 
