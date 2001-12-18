537acfbe1d105668e270785145527a18b8d50c87
Jeremy Siek <jeremy.siek@gmail.com>
2001-12-18 17:19:28 +0000

    added some instructions for running the program
    [SVN r12113]

--- example/max_flow.cpp
+++ example/max_flow.cpp
@@ -31,6 +31,35 @@
 #include <boost/graph/read_dimacs.hpp>
 #include <boost/graph/graph_utility.hpp>
 
+// Use a DIMACS network flow file as stdin.
+// max_flow < max_flow.dat
+//
+// Sample output:
+//  c  The total flow:
+//  s 13
+//
+//  c flow values:
+//  f 0 6 3
+//  f 0 1 6
+//  f 0 2 4
+//  f 1 5 1
+//  f 1 0 0
+//  f 1 3 5
+//  f 2 4 4
+//  f 2 3 0
+//  f 2 0 0
+//  f 3 7 5
+//  f 3 2 0
+//  f 3 1 0
+//  f 4 5 4
+//  f 4 6 0
+//  f 5 4 0
+//  f 5 7 5
+//  f 6 7 3
+//  f 6 4 0
+//  f 7 6 0
+//  f 7 5 0
+
 int
 main()
 {
