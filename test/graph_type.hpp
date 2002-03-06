9b21a3ce6df63e4307aacc9cadd280980db227e7
nobody <nobody@localhost>
2002-03-06 14:13:30 +0000

    This commit was manufactured by cvs2svn to create tag 'perforce_2_4_merge_1'.
    [SVN r13112]

--- test/graph_type.hpp
+++
@@ -1,6 +0,0 @@
-#include <boost/graph/adjacency_list.hpp>
-typedef boost::adjacency_list<boost::setS, boost::setS, boost::undirectedS, 
-  boost::property<vertex_id_t, std::size_t>, 
-  boost::property<edge_id_t, std::size_t> > Graph;
-typedef boost::property<vertex_id_t, std::size_t> VertexId;
-typedef boost::property<edge_id_t, std::size_t> EdgeID;
