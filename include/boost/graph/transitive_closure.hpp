07fe2aafa35034f463b9837bc8cf3585e5cecd7b
Jeremy Siek <jeremy.siek@gmail.com>
2003-10-31 01:30:10 +0000

    untabify
    [SVN r20569]

--- include/boost/graph/transitive_closure.hpp
+++ include/boost/graph/transitive_closure.hpp
@@ -231,15 +231,15 @@ namespace boost
     {
       vertex_iterator i, i_end;
       for (tie(i, i_end) = vertices(g); i != i_end; ++i) 
-	{
+        {
           adjacency_iterator ab, ae;
           for (boost::tie(ab, ae) = adjacent_vertices(*i, g); ab != ae; ++ab)
-	    {
+            {
               if (*ab == *i)
-		if (components[component_number[*i]].size() == 1)
-		  add_edge(g_to_tc_map[*i], g_to_tc_map[*i], tc);
-	    }
-	}
+                if (components[component_number[*i]].size() == 1)
+                  add_edge(g_to_tc_map[*i], g_to_tc_map[*i], tc);
+            }
+        }
     }
   }
 
