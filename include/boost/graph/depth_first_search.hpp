ccd8ecee0956f51ce3e32a35f749f25e891b2cc9
Jeremy Siek <jeremy.siek@gmail.com>
2004-01-21 19:04:09 +0000

    fixed more bugs triggered by the changes in input_iterator_archetype
    [SVN r21867]

--- include/boost/graph/depth_first_search.hpp
+++ include/boost/graph/depth_first_search.hpp
@@ -47,6 +47,7 @@
 #include <boost/graph/named_function_params.hpp>
 
 #include <boost/ref.hpp>
+#include <boost/implicit_cast.hpp>
 
 #include <vector>
 #include <utility>
@@ -212,6 +213,7 @@ namespace boost {
   depth_first_search(const VertexListGraph& g, DFSVisitor vis, ColorMap color,
                      typename graph_traits<VertexListGraph>::vertex_descriptor start_vertex)
   {
+    typedef typename graph_traits<VertexListGraph>::vertex_descriptor Vertex;
     function_requires<DFSVisitorConcept<DFSVisitor, VertexListGraph> >();
     typedef typename property_traits<ColorMap>::value_type ColorValue;
     typedef color_traits<ColorValue> Color;
@@ -221,7 +223,7 @@ namespace boost {
       put(color, *ui, Color::white());       vis.initialize_vertex(*ui, g);
     }
 
-    if (start_vertex != *vertices(g).first){ vis.start_vertex(start_vertex, g);
+    if (start_vertex != implicit_cast<Vertex>(*vertices(g).first)){ vis.start_vertex(start_vertex, g);
       detail::depth_first_visit_impl(g, start_vertex, vis, color,
                                      detail::nontruth2());
     }
