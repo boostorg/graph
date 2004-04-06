b958e455d264949fe3d36fabd58ff32a5ad0392c
Jeremy Siek <jeremy.siek@gmail.com>
2004-04-06 03:00:14 +0000

    added note at the top
    [SVN r2107]

--- test/digraph.cpp
+++ test/digraph.cpp
@@ -1,6 +1,13 @@
+// Copyright Jeremy Siek 2004
+
+// This file was an experiment to try supporting operator-> to access
+// vertex and edge properties from a vertex or descriptor.  The
+// experiment was abandoned because of const issues.
+
 #include <string>
 #include <iostream>
 #include <boost/graph/digraph.hpp>
+#include <boost/graph/graph_concepts.hpp>
 
 struct city;
 struct road;
@@ -20,7 +27,13 @@ int main()
   city jersey("New Jersey");
 
   graph_t::vertex_descriptor v = add_vertex(jersey, g);
-  std::cout << "v->name: " << v->name << std::endl;
+  std::cout << "v->name: " << v->name 
+            << std::endl
+            << "v.index(): " << v.index()
+            << std::endl;
+
+  boost::function_requires<boost::IncidenceGraphConcept<graph_t> >();
+  boost::function_requires<boost::AdjacencyGraphConcept<graph_t> >();
   
   return 0;
 }
