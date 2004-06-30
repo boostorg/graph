4c469946fe0cba6d4a2c6197ae2fa463626774c3
Jeremy Siek <jeremy.siek@gmail.com>
2004-06-30 16:08:33 +0000

    added a second test with no properties
    [SVN r23287]

--- test/bidir_remove_edge.cpp
+++ test/bidir_remove_edge.cpp
@@ -1,27 +1,41 @@
 #include <iostream>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/cstdlib.hpp>
+#include <boost/test/test_tools.hpp>
 
 struct edge_prop {
-  float weight;
+  int weight;
 };
 
 int
 main()
 {
-  typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS,
-    boost::no_property, edge_prop> graph;
-  typedef boost::graph_traits<graph>::edge_descriptor edge;
+  {
+    typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS,
+      boost::no_property, edge_prop> graph;
+    typedef boost::graph_traits<graph>::edge_descriptor edge;
 
-  graph g(2);
+    graph g(2);
 
-  edge_prop p = { 3.14159 };
-  edge e; bool b;
-  tie(e, b) = add_edge(0, 1, p, g);
-  std::cout << "num_edges: " << num_edges(g) << std::endl;
-  
-  remove_edge(e, g);
-  std::cout << "num_edges: " << num_edges(g) << std::endl;
+    edge_prop p = { 42 };
+    edge e; bool b;
+    tie(e, b) = add_edge(0, 1, p, g);
+    assert( num_edges(g) == 1 );
+    assert( g[e].weight == 42 );
+    remove_edge(e, g);
+    assert( num_edges(g) == 0 );
+  }
+  {
+    typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS> graph;
+    typedef boost::graph_traits<graph>::edge_descriptor edge;
 
+    graph g(2);
+
+    edge e; bool b;
+    tie(e, b) = add_edge(0, 1, g);
+    assert( num_edges(g) == 1 );
+    remove_edge(e, g);
+    assert( num_edges(g) == 0 );
+  }
   return boost::exit_success;
 }
