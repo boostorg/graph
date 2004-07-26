34fefcd0f6233a63fe93dec7bcaed38e95568673
Douglas Gregor <doug.gregor@gmail.com>
2004-07-26 00:32:12 +0000

    Converted to Boost Software License, Version 1.0
    [SVN r24055]

--- include/boost/graph/bandwidth.hpp
+++ include/boost/graph/bandwidth.hpp
@@ -1,12 +1,8 @@
 // Copyright (c) Jeremy Siek 2001, Marc Wintermantel 2002
 //
-// Permission to use, copy, modify, distribute and sell this software
-// and its documentation for any purpose is hereby granted without fee,
-// provided that the above copyright notice appears in all copies and
-// that both that copyright notice and this permission notice appear
-// in supporting documentation.  Silicon Graphics makes no
-// representations about the suitability of this software for any
-// purpose.  It is provided "as is" without express or implied warranty.
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef BOOST_GRAPH_BANDWIDTH_HPP
 #define BOOST_GRAPH_BANDWIDTH_HPP
