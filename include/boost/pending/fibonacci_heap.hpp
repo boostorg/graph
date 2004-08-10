5de49c9ea3a43a10ca13dec24adda7d0f14ea869
Jeremy Siek <jeremy.siek@gmail.com>
2004-08-10 18:44:34 +0000

    added a copyright
    [SVN r24393]

--- include/boost/pending/fibonacci_heap.hpp
+++ include/boost/pending/fibonacci_heap.hpp
@@ -1,3 +1,9 @@
+// (C) Copyright Jeremy Siek    2004.
+// Permission to copy, use, modify,
+// sell and distribute this software is granted provided this
+// copyright notice appears in all copies. This software is provided
+// "as is" without express or implied warranty, and with no claim as
+// to its suitability for any purpose.
 #ifndef BOOST_FIBONACCI_HEAP_HPP
 #define BOOST_FIBONACCI_HEAP_HPP
 
