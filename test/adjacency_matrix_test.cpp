e0356c30ca1f4427242851bd6080eb4dd08ef0c4
Douglas Gregor <doug.gregor@gmail.com>
2004-08-12 19:12:28 +0000

    Moved to BSL
    [SVN r24450]

--- test/adjacency_matrix_test.cpp
+++ test/adjacency_matrix_test.cpp
@@ -2,14 +2,9 @@
  *
  * Copyright Cromwell D. Enage 2004
  *
- * Permission to use, copy, modify, sell, and distribute this software
- * is hereby granted without fee provided that the above copyright notice
- * appears in all copies and that both that copyright notice and this
- * permission notice appear in supporting documentation,
- *
- * Cromwell D. Enage makes no representations about the suitability of this
- * software for any purpose. It is provided "as is" without express or
- * implied warranty.
+ * Distributed under the Boost Software License, Version 1.0. (See
+ * accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
  */
 
 /*
