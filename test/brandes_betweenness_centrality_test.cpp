3082cb519542f185c3aab117b989ae7eec8bbb01
Douglas Gregor <doug.gregor@gmail.com>
2004-08-16 13:59:51 +0000

    Add copyright/license
    [SVN r24503]

--- test/brandes_betweenness_centrality_test.cpp
+++ test/brandes_betweenness_centrality_test.cpp
@@ -1,3 +1,11 @@
+// Copyright 2004 The Trustees of Indiana University.
+
+// Use, modification and distribution is subject to the Boost Software
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+//  Authors: Douglas Gregor
+//           Andrew Lumsdaine
 #include <boost/graph/brandes_betweenness_centrality.hpp>
 
 #include <boost/graph/adjacency_list.hpp>
