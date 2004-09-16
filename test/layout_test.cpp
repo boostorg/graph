3b9071cf2739496772292eb48446584c09ef7e08
Douglas Gregor <doug.gregor@gmail.com>
2004-09-16 19:58:18 +0000

    Stupid limits include
    [SVN r25159]

--- test/layout_test.cpp
+++ test/layout_test.cpp
@@ -11,7 +11,7 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/test/minimal.hpp>
 #include <iostream>
-#include <limits>
+#include <boost/limits.hpp>
 #include <fstream>
 #include <string>
 using namespace boost;
