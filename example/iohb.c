9197a1028f1b89287d71404771f36ca0b3c72502
Jeremy Siek <jeremy.siek@gmail.com>
2004-10-20 13:20:19 +0000

    added copyrights
    [SVN r25810]

--- example/iohb.c
+++ example/iohb.c
@@ -1,3 +1,8 @@
+//  (C) Copyright Jeremy Siek 2004 
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
 /*
 Fri Aug 15 16:29:47 EDT 1997
 
--- test/transitive_closure_test2.cpp
+++ test/transitive_closure_test2.cpp
@@ -1,3 +1,7 @@
+// Copyright (C) 2004 Jeremy Siek <jsiek@cs.indiana.edu>
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/depth_first_search.hpp>
 #include <boost/graph/transitive_closure.hpp>
