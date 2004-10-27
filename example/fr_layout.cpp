4a11e36f85ad1f639e3a7bc8a53ec4acba234426
Douglas Gregor <doug.gregor@gmail.com>
2004-10-27 18:22:57 +0000

    Document FR example
    [SVN r25904]

--- example/fr_layout.cpp
+++ example/fr_layout.cpp
@@ -1,3 +1,11 @@
+// Copyright 2004 The Trustees of Indiana University.
+
+// Use, modification and distribution is subject to the Boost Software
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+//  Authors: Douglas Gregor
+//           Andrew Lumsdaine
 #include <boost/graph/fruchterman_reingold.hpp>
 #include <boost/graph/random_layout.hpp>
 #include <boost/graph/adjacency_list.hpp>
