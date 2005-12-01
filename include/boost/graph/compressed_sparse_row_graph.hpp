549f85c80fbed7934daa8ce10ed0b40f0d60fe5f
Douglas Gregor <doug.gregor@gmail.com>
2005-12-01 18:24:00 +0000

    Compressed sparse row graph type
    [SVN r31865]

---
+++ include/boost/graph/compressed_sparse_row_graph.hpp
@@ -0,0 +1,490 @@
+// Copyright 2005 The Trustees of Indiana University.
+
+// Use, modification and distribution is subject to the Boost Software
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+//  Authors: Jeremiah Willcock
+//           Douglas Gregor
+//           Andrew Lumsdaine
+
+// Compressed sparse row graph type
+
+#ifndef BOOST_GRAPH_COMPRESSED_SPARSE_ROW_GRAPH_HPP
+#define BOOST_GRAPH_COMPRESSED_SPARSE_ROW_GRAPH_HPP
+
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <climits>
+#include <iterator>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/properties.hpp>
+#include <boost/iterator/counting_iterator.hpp>
+#include <boost/integer.hpp>
+#include <boost/iterator/iterator_facade.hpp>
+
+namespace boost {
+
+// A tag type indicating that the graph in question is a compressed
+// sparse row graph. This is an internal detail of the BGL.
+struct csr_graph_tag;
+
+/** Compressed sparse row graph.
+ *
+ * Vertex and EdgeIndex should be unsigned integral types and should
+ * specialize numeric_limits.
+ *
+ * DPG TBD: Shall we take vertex/edge properties?
+ */
+template<typename Vertex = std::size_t, typename EdgeIndex = Vertex>
+class compressed_sparse_row_graph
+{
+ protected:
+  template<typename InputIterator>
+  void
+  maybe_reserve_edge_list_storage(InputIterator, InputIterator,
+                                  std::input_iterator_tag)
+  {
+    // Do nothing: we have no idea how much storage to reserve.
+  }
+
+  template<typename InputIterator>
+  void
+  maybe_reserve_edge_list_storage(InputIterator first, InputIterator last,
+                                  std::forward_iterator_tag)
+  {
+    using std::distance;
+    m_column.reserve(distance(first, last));
+  }
+
+ public:
+  // Concept requirements:
+  // For Graph
+  typedef Vertex vertex_descriptor;
+  class edge_descriptor;
+  typedef directed_tag directed_category;
+  typedef allow_parallel_edge_tag edge_parallel_category;
+
+  class traversal_category: public incidence_graph_tag,
+                            public adjacency_graph_tag,
+                            public vertex_list_graph_tag,
+                            public edge_list_graph_tag {};
+
+  static vertex_descriptor null_vertex() { return vertex_descriptor(-1); }
+
+  // For VertexListGraph
+  typedef counting_iterator<Vertex> vertex_iterator;
+  typedef Vertex vertices_size_type;
+
+  // For EdgeListGraph
+  typedef EdgeIndex edges_size_type;
+
+  // For IncidenceGraph
+  class out_edge_iterator;
+  typedef EdgeIndex degree_size_type;
+
+  // For AdjacencyGraph
+  typedef typename std::vector<Vertex>::const_iterator adjacency_iterator;
+
+  // For EdgeListGraph
+  class edge_iterator;
+
+  // For BidirectionalGraph (not implemented)
+  typedef void in_edge_iterator;
+
+  // For internal use
+  typedef no_property vertex_property_type;
+  typedef no_property edge_property_type;
+  typedef csr_graph_tag graph_tag;
+
+  // Constructors
+
+  // Default constructor: an empty graph.
+  compressed_sparse_row_graph(): m_rowstart(1), m_column(0) {}
+
+  //  From number of vertices and sorted list of edges
+  template<typename InputIterator>
+  compressed_sparse_row_graph(vertices_size_type numverts,
+                              InputIterator edge_begin, InputIterator edge_end,
+                              edges_size_type numedges = 0)
+    : m_rowstart(numverts + 1), m_column(0)
+  {
+    // Reserving storage in advance can save us lots of time and
+    // memory, but it can only be done if we have forward iterators or
+    // the user has supplied the number of edges.
+    if (numedges == 0) {
+      typedef typename std::iterator_traits<InputIterator>::iterator_category
+        category;
+      maybe_reserve_edge_list_storage(edge_begin, edge_end, category());
+    } else {
+      m_column.reserve(numedges);
+    }
+
+    EdgeIndex current_edge = 0;
+    Vertex current_vertex_plus_one = 1;
+    m_rowstart[0] = 0;
+    for (InputIterator ei = edge_begin; ei != edge_end; ++ei) {
+      Vertex src = ei->first;
+      Vertex tgt = ei->second;
+      for (; current_vertex_plus_one != src + 1; ++current_vertex_plus_one)
+        m_rowstart[current_vertex_plus_one] = current_edge;
+      m_column.push_back(tgt);
+      ++current_edge;
+    }
+
+    // The remaining vertices have no edges
+    for (; current_vertex_plus_one != numverts + 1; ++current_vertex_plus_one)
+      m_rowstart[current_vertex_plus_one] = current_edge;
+  }
+
+  //   Requires IncidenceGraph, a vertex index map, and a vertex(n, g) function
+  template<typename Graph, typename VertexIndexMap>
+  compressed_sparse_row_graph(const Graph& g, const VertexIndexMap& vi,
+                              vertices_size_type numverts,
+                              edges_size_type numedges)
+  {
+    assign(g, vi, numverts, numedges);
+  }
+
+  //   Requires VertexListGraph and EdgeListGraph
+  template<typename Graph, typename VertexIndexMap>
+  compressed_sparse_row_graph(const Graph& g, const VertexIndexMap& vi)
+  {
+    assign(g, vi, num_vertices(g), num_edges(g));
+  }
+
+  // Requires vertex index map plus requirements of previous constructor
+  template<typename Graph>
+  explicit compressed_sparse_row_graph(const Graph& g)
+  {
+    assign(g, get(vertex_index, g), num_vertices(g), num_edges(g));
+  }
+
+  // From any graph (slow and uses a lot of memory)
+  //   Requires IncidenceGraph, a vertex index map, and a vertex(n, g) function
+  //   Internal helper function
+  template<typename Graph, typename VertexIndexMap>
+  void
+  assign(const Graph& g, const VertexIndexMap& vi,
+         vertices_size_type numverts, edges_size_type numedges)
+  {
+    m_rowstart.resize(numverts + 1);
+    m_column.resize(numedges);
+    EdgeIndex current_edge = 0;
+    typedef typename boost::graph_traits<Graph>::vertex_descriptor g_vertex;
+    typedef typename boost::graph_traits<Graph>::edge_descriptor g_edge;
+    typedef typename boost::graph_traits<Graph>::out_edge_iterator
+      g_out_edge_iter;
+
+    for (Vertex i = 0; i != numverts; ++i) {
+      m_rowstart[i] = current_edge;
+      g_vertex v = vertex(i, g);
+      std::vector<Vertex> current_out_edges(out_degree(v, g));
+      g_out_edge_iter ei, ei_end;
+      EdgeIndex x = 0;
+      for (tie(ei, ei_end) = out_edges(vertex(i, g), g); ei != ei_end; ++ei) {
+        current_out_edges[x++] = get(vi, target(*ei, g));
+      }
+      std::sort(current_out_edges.begin(), current_out_edges.end());
+      for (x = 0; x != current_out_edges.size(); ++x, ++current_edge)
+        m_column[current_edge] = current_out_edges[x];
+    }
+    m_rowstart[numverts] = current_edge;
+  }
+
+  // Requires the above, plus VertexListGraph and EdgeListGraph
+  template<typename Graph, typename VertexIndexMap>
+  void assign(const Graph& g, const VertexIndexMap& vi)
+  {
+    assign(g, vi, num_vertices(g), num_edges(g));
+  }
+
+  // Requires the above, plus a vertex_index map.
+  template<typename Graph>
+  void assign(const Graph& g)
+  {
+    assign(g, get(vertex_index, g), num_vertices(g), num_edges(g));
+  }
+
+  // private: non-portable, requires friend templates
+  std::vector<EdgeIndex> m_rowstart;
+  std::vector<Vertex> m_column;
+};
+
+template<typename Vertex, typename EdgeIndex>
+class compressed_sparse_row_graph<Vertex, EdgeIndex>::edge_descriptor
+{
+ public:
+  Vertex src;
+  EdgeIndex idx;
+
+  edge_descriptor(Vertex src, EdgeIndex idx): src(src), idx(idx) {}
+
+  edge_descriptor(): src(0), idx(0) {}
+
+  bool operator==(const edge_descriptor& o) const {return idx == o.idx;}
+  bool operator!=(const edge_descriptor& o) const {return idx != o.idx;}
+  bool operator<(const edge_descriptor& e) const {return idx < e.idx;}
+  bool operator>(const edge_descriptor& e) const {return idx > e.idx;}
+  bool operator<=(const edge_descriptor& e) const {return idx <= e.idx;}
+  bool operator>=(const edge_descriptor& e) const {return idx >= e.idx;}
+};
+
+// From VertexListGraph
+template<typename Vertex, typename EdgeIndex>
+inline Vertex
+num_vertices(const compressed_sparse_row_graph<Vertex, EdgeIndex>& g) {
+  return g.m_rowstart.size() - 1;
+}
+
+template<typename Vertex, typename EdgeIndex>
+std::pair<counting_iterator<Vertex>, counting_iterator<Vertex> >
+inline vertices(const compressed_sparse_row_graph<Vertex, EdgeIndex>& g) {
+  return std::make_pair(counting_iterator<Vertex>(0),
+                        counting_iterator<Vertex>(num_vertices(g)));
+}
+
+// From IncidenceGraph
+template<typename Vertex, typename EdgeIndex>
+class compressed_sparse_row_graph<Vertex, EdgeIndex>::out_edge_iterator
+  : public iterator_facade<out_edge_iterator,
+                           edge_descriptor,
+                           std::random_access_iterator_tag,
+                           const edge_descriptor&,
+                           typename int_t<CHAR_BIT * sizeof(EdgeIndex)>::fast>
+{
+ public:
+  typedef typename int_t<CHAR_BIT * sizeof(EdgeIndex)>::fast difference_type;
+
+  out_edge_iterator() {}
+  // Implicit copy constructor OK
+  explicit out_edge_iterator(edge_descriptor edge) : m_edge(edge) { }
+
+ private:
+  // iterator_facade requirements
+  const edge_descriptor& dereference() const { return m_edge; }
+
+  bool equal(const out_edge_iterator& other) const
+  { return m_edge == other.m_edge; }
+
+  void increment() { ++m_edge.idx; }
+  void decrement() { ++m_edge.idx; }
+  void advance(difference_type n) { m_edge.idx += n; }
+
+  difference_type distance_to(const out_edge_iterator& other) const
+  { return other.m_edge.idx - m_edge.idx; }
+
+  edge_descriptor m_edge;
+
+  friend class iterator_core_access;
+};
+
+template<typename Vertex, typename EdgeIndex>
+inline Vertex
+source(typename compressed_sparse_row_graph<Vertex, EdgeIndex>::edge_descriptor e,
+       const compressed_sparse_row_graph<Vertex, EdgeIndex>&)
+{
+  return e.src;
+}
+
+template<typename Vertex, typename EdgeIndex>
+inline Vertex
+target(typename compressed_sparse_row_graph<Vertex, EdgeIndex>::edge_descriptor e,
+       const compressed_sparse_row_graph<Vertex, EdgeIndex>& g)
+{
+  return g.m_column[e.idx];
+}
+
+template<typename Vertex, typename EdgeIndex>
+inline std::pair<typename compressed_sparse_row_graph<Vertex, EdgeIndex>::out_edge_iterator,
+                 typename compressed_sparse_row_graph<Vertex, EdgeIndex>::out_edge_iterator>
+out_edges(Vertex v, const compressed_sparse_row_graph<Vertex, EdgeIndex>& g)
+{
+  typedef typename compressed_sparse_row_graph<Vertex, EdgeIndex>::edge_descriptor ed;
+  typedef typename compressed_sparse_row_graph<Vertex, EdgeIndex>::out_edge_iterator it;
+  return std::make_pair(it(ed(v, g.m_rowstart[v])),
+                        it(ed(v, g.m_rowstart[v + 1])));
+}
+
+template<typename Vertex, typename EdgeIndex>
+inline EdgeIndex
+out_degree(Vertex v, const compressed_sparse_row_graph<Vertex, EdgeIndex>& g)
+{
+  return g.m_rowstart[v + 1] - g.m_rowstart[v];
+}
+
+// From AdjacencyGraph
+template<typename Vertex, typename EdgeIndex>
+inline std::pair<typename compressed_sparse_row_graph<Vertex, EdgeIndex>::adjacency_iterator,
+                 typename compressed_sparse_row_graph<Vertex, EdgeIndex>::adjacency_iterator>
+adjacent_vertices(Vertex v, const compressed_sparse_row_graph<Vertex, EdgeIndex>& g)
+{
+  return std::make_pair(g.m_column.begin() + g.m_rowstart[v],
+                        g.m_column.begin() + g.m_rowstart[v + 1]);
+}
+
+// Extra, common function
+template<typename Vertex, typename EdgeIndex>
+inline Vertex
+vertex(Vertex i, const compressed_sparse_row_graph<Vertex, EdgeIndex>&)
+{
+  return i;
+}
+
+// From EdgeListGraph
+template<typename Vertex, typename EdgeIndex>
+class compressed_sparse_row_graph<Vertex, EdgeIndex>::edge_iterator
+{
+ public:
+  typedef std::forward_iterator_tag iterator_category;
+  typedef edge_descriptor value_type;
+
+  class pointer
+  {
+  public:
+    explicit pointer(const edge_descriptor& edge) : edge(edge) { }
+    const edge_descriptor* operator->() const { return &edge; }
+
+  private:
+    edge_descriptor edge;
+  };
+
+  typedef edge_descriptor reference;
+  typedef typename int_t<CHAR_BIT * sizeof(EdgeIndex)>::fast difference_type;
+
+  edge_iterator() : rowstart_array(0), src(0), idx(0), end_of_this_vertex(0) {}
+
+  edge_iterator(const compressed_sparse_row_graph& graph, Vertex src,
+                EdgeIndex idx, EdgeIndex end_of_this_vertex)
+    : rowstart_array(&graph.m_rowstart[0]), src(src), idx(idx),
+      end_of_this_vertex(end_of_this_vertex) {}
+
+  // From InputIterator
+  reference operator*() const { return edge_descriptor(src, idx); }
+  pointer operator->() const { return pointer(**this); }
+
+  bool operator==(const edge_iterator& o) const {return idx == o.idx;}
+  bool operator!=(const edge_iterator& o) const {return idx != o.idx;}
+
+  edge_iterator& operator++() {
+    ++idx;
+    while (idx == end_of_this_vertex) {
+      ++src;
+      end_of_this_vertex = rowstart_array[src + 1];
+    }
+    return *this;
+  }
+
+  edge_iterator operator++(int) {
+    edge_iterator temp = *this;
+    ++*this;
+    return temp;
+  }
+
+ private:
+  const EdgeIndex* rowstart_array;
+  Vertex src;
+  EdgeIndex idx;
+  EdgeIndex end_of_this_vertex;
+};
+
+template<typename Vertex, typename EdgeIndex>
+inline EdgeIndex
+num_edges(const compressed_sparse_row_graph<Vertex, EdgeIndex>& g)
+{
+  return g.m_column.size();
+}
+
+template<typename Vertex, typename EdgeIndex>
+std::pair<typename compressed_sparse_row_graph<Vertex, EdgeIndex>::edge_iterator,
+          typename compressed_sparse_row_graph<Vertex, EdgeIndex>::edge_iterator>
+edges(const compressed_sparse_row_graph<Vertex, EdgeIndex>& g)
+{
+  typedef typename compressed_sparse_row_graph<Vertex, EdgeIndex>::edge_iterator ei;
+  if (g.m_rowstart.size() == 1) {
+    return std::make_pair(ei(), ei());
+  } else {
+    // Find the first vertex that has outgoing edges
+    Vertex src = 0;
+    while (g.m_rowstart[src + 1] == 0) ++ src;
+    return std::make_pair(ei(g, src, 0, g.m_rowstart[src + 1]),
+                          ei(g, num_vertices(g), g.m_rowstart[num_vertices(g)],
+                             0));
+  }
+}
+
+// Add vertex_index and edge_index property maps
+template<typename Vertex, typename EdgeIndex>
+struct csr_edge_index_map
+{
+  typedef EdgeIndex value_type;
+  typedef EdgeIndex reference;
+  typedef typename compressed_sparse_row_graph<Vertex, EdgeIndex>
+                     ::edge_descriptor key_type;
+  typedef readable_property_map_tag category;
+};
+
+template<typename Vertex, typename EdgeIndex>
+inline EdgeIndex
+get(const csr_edge_index_map<Vertex, EdgeIndex>&,
+    const typename csr_edge_index_map<Vertex, EdgeIndex>::key_type& key)
+{
+  return key.idx;
+}
+
+// Doing the right thing here (by unifying with vertex_index_t and
+// edge_index_t) breaks GCC.
+template<typename Vertex, typename EdgeIndex, typename Tag>
+struct property_map<compressed_sparse_row_graph<Vertex, EdgeIndex>, Tag>
+{
+private:
+  typedef identity_property_map vertex_index_type;
+  typedef csr_edge_index_map<Vertex, EdgeIndex> edge_index_type;
+
+  typedef typename mpl::if_<is_same<Tag, edge_index_t>,
+                            edge_index_type,
+                            detail::error_property_not_found>::type
+    edge_or_none;
+
+public:
+  typedef typename mpl::if_<is_same<Tag, vertex_index_t>,
+                            vertex_index_type,
+                            edge_or_none>::type type;
+
+  typedef type const_type;
+};
+
+template<typename Vertex, typename EdgeIndex>
+inline identity_property_map
+get(vertex_index_t, const compressed_sparse_row_graph<Vertex, EdgeIndex>&)
+{
+  return identity_property_map();
+}
+
+template<typename Vertex, typename EdgeIndex>
+inline Vertex
+get(vertex_index_t,
+    const compressed_sparse_row_graph<Vertex, EdgeIndex>&, Vertex v)
+{
+  return v;
+}
+
+template<typename Vertex, typename EdgeIndex>
+inline csr_edge_index_map<Vertex, EdgeIndex>
+get(edge_index_t, const compressed_sparse_row_graph<Vertex, EdgeIndex>&)
+{
+  return csr_edge_index_map<Vertex, EdgeIndex>();
+}
+
+template<typename Vertex, typename EdgeIndex>
+inline EdgeIndex
+get(edge_index_t, const compressed_sparse_row_graph<Vertex, EdgeIndex>&,
+    typename compressed_sparse_row_graph<Vertex, EdgeIndex>::edge_descriptor e)
+{
+  return e.idx;
+}
+
+} // end namespace boost
+
+#endif // BOOST_GRAPH_COMPRESSED_SPARSE_ROW_GRAPH_HPP
---
+++ test/csr_graph_test.cpp
@@ -0,0 +1,204 @@
+// Copyright 2005 The Trustees of Indiana University.
+
+// Use, modification and distribution is subject to the Boost Software
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+//  Authors: Jeremiah Willcock
+//           Douglas Gregor
+//           Andrew Lumsdaine
+
+// Test for the compressed sparse row graph type
+#include <boost/graph/compressed_sparse_row_graph.hpp>
+#include <boost/test/minimal.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/erdos_renyi_generator.hpp>
+#include <boost/graph/graph_utility.hpp>
+#include <boost/random/linear_congruential.hpp>
+#include <cassert>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include <boost/lexical_cast.hpp>
+#include <boost/iterator/transform_iterator.hpp>
+#include <boost/limits.hpp>
+
+// Algorithms to test against
+#include <boost/graph/betweenness_centrality.hpp>
+#include <boost/graph/kruskal_min_spanning_tree.hpp>
+
+typedef boost::adjacency_list<> Graph;
+typedef boost::erdos_renyi_iterator<boost::minstd_rand, Graph> ERGen;
+
+typedef boost::compressed_sparse_row_graph<> CSRGraph;
+
+template <class G1, class VI1, class G2, class VI2, class IsomorphismMap>
+void assert_graphs_equal(const G1& g1, const VI1& vi1,
+                         const G2& g2, const VI2& vi2,
+                         const IsomorphismMap& iso) {
+  BOOST_CHECK (num_vertices(g1) == num_vertices(g2));
+  BOOST_CHECK (num_edges(g1) == num_edges(g2));
+
+  typedef typename boost::graph_traits<G1>::vertex_iterator vertiter1;
+  {
+    vertiter1 i, iend;
+    for (boost::tie(i, iend) = vertices(g1); i != iend; ++i) {
+      typename boost::graph_traits<G1>::vertex_descriptor v1 = *i;
+      typename boost::graph_traits<G2>::vertex_descriptor v2 = iso[v1];
+
+      BOOST_CHECK (vi1[v1] == vi2[v2]);
+
+      BOOST_CHECK (out_degree(v1, g1) == out_degree(v2, g2));
+      std::vector<std::size_t> edges1(out_degree(v1, g1));
+      typename boost::graph_traits<G1>::out_edge_iterator oe1, oe1end;
+      for (boost::tie(oe1, oe1end) = out_edges(v1, g1); oe1 != oe1end; ++oe1) {
+        BOOST_CHECK (source(*oe1, g1) == v1);
+        edges1.push_back(vi1[target(*oe1, g1)]);
+      }
+      std::vector<std::size_t> edges2(out_degree(v2, g2));
+      typename boost::graph_traits<G2>::out_edge_iterator oe2, oe2end;
+      for (boost::tie(oe2, oe2end) = out_edges(v2, g2); oe2 != oe2end; ++oe2) {
+        BOOST_CHECK (source(*oe2, g2) == v2);
+        edges2.push_back(vi2[target(*oe2, g2)]);
+      }
+
+      std::sort(edges1.begin(), edges1.end());
+      std::sort(edges2.begin(), edges2.end());
+      BOOST_CHECK (edges1 == edges2);
+    }
+  }
+
+  {
+    std::vector<std::pair<std::size_t, std::size_t> > all_edges1;
+    std::vector<std::pair<std::size_t, std::size_t> > all_edges2;
+    typename boost::graph_traits<G1>::edge_iterator ei1, ei1end;
+    for (boost::tie(ei1, ei1end) = edges(g1); ei1 != ei1end; ++ei1)
+      all_edges1.push_back(std::make_pair(vi1[source(*ei1, g1)],
+                                          vi1[target(*ei1, g1)]));
+    typename boost::graph_traits<G2>::edge_iterator ei2, ei2end;
+    for (boost::tie(ei2, ei2end) = edges(g2); ei2 != ei2end; ++ei2)
+      all_edges2.push_back(std::make_pair(vi2[source(*ei2, g2)],
+                                          vi2[target(*ei2, g2)]));
+    std::sort(all_edges1.begin(), all_edges1.end());
+    std::sort(all_edges2.begin(), all_edges2.end());
+    BOOST_CHECK (all_edges1 == all_edges2);
+  }
+}
+
+template<typename Graph, typename VertexIndexMap>
+class edge_to_index_pair
+{
+  typedef typename boost::graph_traits<Graph>::vertices_size_type
+    vertices_size_type;
+  typedef typename boost::graph_traits<Graph>::edge_descriptor edge_descriptor;
+
+ public:
+  typedef std::pair<vertices_size_type, vertices_size_type> result_type;
+
+  edge_to_index_pair() : g(0), index() { }
+  edge_to_index_pair(const Graph& g, const VertexIndexMap& index)
+    : g(&g), index(index)
+  { }
+
+  result_type operator()(edge_descriptor e) const
+  {
+    return result_type(get(index, source(e, *g)), get(index, target(e, *g)));
+  }
+
+ private:
+  const Graph* g;
+  VertexIndexMap index;
+};
+
+template<typename Graph, typename VertexIndexMap>
+edge_to_index_pair<Graph, VertexIndexMap>
+make_edge_to_index_pair(const Graph& g, const VertexIndexMap& index)
+{
+  return edge_to_index_pair<Graph, VertexIndexMap>(g, index);
+}
+
+template<typename Graph>
+edge_to_index_pair
+  <Graph,
+   typename boost::property_map<Graph,boost::vertex_index_t>::const_type>
+make_edge_to_index_pair(const Graph& g)
+{
+  typedef typename boost::property_map<Graph,
+                                       boost::vertex_index_t>::const_type
+    VertexIndexMap;
+  return edge_to_index_pair<Graph, VertexIndexMap>(g,
+                                                   get(boost::vertex_index,
+                                                       g));
+}
+
+void test(int nnodes, double density, int seed)
+{
+  std::cout << "Seed = " << seed << std::endl;
+
+  boost::minstd_rand gen(seed);
+  std::cout << "Testing " << nnodes << " density " << density << std::endl;
+
+  // Check copying of a graph
+  Graph g(ERGen(gen, nnodes, density), ERGen(), nnodes);
+  CSRGraph g2(g);
+  BOOST_CHECK((std::size_t)std::distance(edges(g2).first, edges(g2).second)
+              == num_edges(g2));
+  assert_graphs_equal(g, boost::identity_property_map(),
+                      g2, boost::identity_property_map(),
+                      boost::identity_property_map());
+
+  // Check constructing a graph from iterators
+  CSRGraph g3(nnodes,
+              boost::make_transform_iterator(edges(g2).first,
+                                             make_edge_to_index_pair(g2)),
+              boost::make_transform_iterator(edges(g2).second,
+                                             make_edge_to_index_pair(g2)));
+  BOOST_CHECK((std::size_t)std::distance(edges(g3).first, edges(g3).second)
+              == num_edges(g3));
+  assert_graphs_equal(g2, boost::identity_property_map(),
+                      g3, boost::identity_property_map(),
+                      boost::identity_property_map());
+
+  // Run brandes_betweenness_centrality, which touches on a whole lot
+  // of things, including VertexListGraph and IncidenceGraph
+  using namespace boost;
+  std::vector<double> vertex_centralities(num_vertices(g3));
+  std::vector<double> edge_centralities(num_edges(g3));
+  brandes_betweenness_centrality
+    (g3,
+     make_iterator_property_map(vertex_centralities.begin(),
+                                get(vertex_index, g3)),
+     make_iterator_property_map(edge_centralities.begin(),
+                                get(edge_index, g3)));
+
+  // Invert the edge centralities and use these as weights to
+  // Kruskal's MST algorithm, which will test the EdgeListGraph
+  // capabilities.
+  double max_val = (std::numeric_limits<double>::max)();
+  for (std::size_t i = 0; i < num_edges(g3); ++i)
+    edge_centralities[i] =
+      edge_centralities[i] == 0.0? max_val : 1.0 / edge_centralities[i];
+
+  typedef graph_traits<CSRGraph>::edge_descriptor edge_descriptor;
+  std::vector<edge_descriptor> mst_edges;
+  mst_edges.reserve(num_vertices(g3));
+  kruskal_minimum_spanning_tree
+    (g3, std::back_inserter(mst_edges),
+     weight_map(make_iterator_property_map(edge_centralities.begin(),
+                                           get(edge_index, g3))));
+}
+
+int test_main(int argc, char* argv[])
+{
+  // Optionally accept a seed value
+  int seed = time(0);
+  if (argc > 1) seed = boost::lexical_cast<int>(argv[1]);
+
+  test(1000, 0.05, seed);
+  test(1000, 0.0, seed);
+  test(1000, 0.1, seed);
+  test(1000, 0.001, seed);
+
+  return 0;
+}
