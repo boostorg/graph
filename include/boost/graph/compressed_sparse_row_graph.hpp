9df2b90f560b26120e060c9dd5b4d772643da729
Jeremiah Willcock <jewillco@osl.iu.edu>
2006-01-06 19:20:38 +0000

    Refactored indexed property code into separate file
    [SVN r32242]

--- include/boost/graph/compressed_sparse_row_graph.hpp
+++ include/boost/graph/compressed_sparse_row_graph.hpp
@@ -20,6 +20,7 @@
 #include <iterator>
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/properties.hpp>
+#include <boost/graph/detail/indexed_properties.hpp>
 #include <boost/iterator/counting_iterator.hpp>
 #include <boost/integer.hpp>
 #include <boost/iterator/iterator_facade.hpp>
@@ -31,142 +32,6 @@
 #endif
 
 namespace boost {
-
-template<typename Derived, typename Property, typename Descriptor>
-class indexed_vertex_properties
-{
-public:
-  typedef no_property vertex_property_type;
-  typedef Property vertex_bundled;
-
-  // Directly access a vertex or edge bundle
-  Property& operator[](Descriptor v)
-  { return m_vertex_properties[get(vertex_index, derived(), v)]; }
-
-  const Property& operator[](Descriptor v) const
-  { return m_vertex_properties[get(vertex_index, derived(), v)]; }
-
-protected:
-  // Default-construct with no property values
-  indexed_vertex_properties() {}
-
-  // Initialize with n default-constructed property values
-  indexed_vertex_properties(std::size_t n) : m_vertex_properties(n) { }
-
-  // Resize the properties vector
-  void resize(std::size_t n)
-  {
-    m_vertex_properties.resize(n);
-  }
-
-  // Reserve space in the vector of properties
-  void reserve(std::size_t n)
-  {
-    m_vertex_properties.reserve(n);
-  }
-
-  // Add a new property value to the back
-  void push_back(const Property& prop)
-  {
-    m_vertex_properties.push_back(prop);
-  }
-
-  // Access to the derived object
-  Derived& derived() { return *static_cast<Derived*>(this); }
-
-  const Derived& derived() const
-  { return *static_cast<const Derived*>(this); }
-
-public: // should be private, but friend templates not portable
-  std::vector<Property> m_vertex_properties;
-};
-
-template<typename Derived, typename Descriptor>
-class indexed_vertex_properties<Derived, void, Descriptor>
-{
-  struct secret {};
-
- public:
-  typedef no_property vertex_property_type;
-  typedef void vertex_bundled;
-
-  secret operator[](secret) { return secret(); }
-
- protected:
-  // All operations do nothing.
-  indexed_vertex_properties() { }
-  indexed_vertex_properties(std::size_t) { }
-  void resize(std::size_t) { }
-  void reserve(std::size_t) { }
-};
-
-template<typename Derived, typename Property, typename Descriptor>
-class indexed_edge_properties
-{
-public:
-  typedef no_property edge_property_type;
-  typedef Property edge_bundled;
-
-  // Directly access a edge or edge bundle
-  Property& operator[](Descriptor v)
-  { return m_edge_properties[get(edge_index, derived(), v)]; }
-
-  const Property& operator[](Descriptor v) const
-  { return m_edge_properties[get(edge_index, derived(), v)]; }
-
-protected:
-  // Default-construct with no property values
-  indexed_edge_properties() {}
-
-  // Initialize with n default-constructed property values
-  indexed_edge_properties(std::size_t n) : m_edge_properties(n) { }
-
-  // Resize the properties vector
-  void resize(std::size_t n)
-  {
-    m_edge_properties.resize(n);
-  }
-
-  // Reserve space in the vector of properties
-  void reserve(std::size_t n)
-  {
-    m_edge_properties.reserve(n);
-  }
-
-  // Add a new property value to the back
-  void push_back(const Property& prop)
-  {
-    m_edge_properties.push_back(prop);
-  }
-
-  // Access to the derived object
-  Derived& derived() { return *static_cast<Derived*>(this); }
-
-  const Derived& derived() const
-  { return *static_cast<const Derived*>(this); }
-
-public: // should be private, but friend templates not portable
-  std::vector<Property> m_edge_properties;
-};
-
-template<typename Derived, typename Descriptor>
-class indexed_edge_properties<Derived, void, Descriptor>
-{
-  struct secret {};
-
- public:
-  typedef no_property edge_property_type;
-  typedef void edge_bundled;
-
-  secret operator[](secret) { return secret(); }
-
- protected:
-  // All operations do nothing.
-  indexed_edge_properties() { }
-  indexed_edge_properties(std::size_t) { }
-  void resize(std::size_t) { }
-  void reserve(std::size_t) { }
-};
 
 // A tag type indicating that the graph in question is a compressed
 // sparse row graph. This is an internal detail of the BGL.
@@ -198,18 +63,19 @@ template<typename VertexProperty = void,
          typename Vertex = std::size_t,
          typename EdgeIndex = Vertex>
 class compressed_sparse_row_graph
-   : public indexed_vertex_properties<BOOST_CSR_GRAPH_TYPE, VertexProperty,
-                                      Vertex>,
-     public indexed_edge_properties<BOOST_CSR_GRAPH_TYPE, EdgeProperty,
-                                    csr_edge_descriptor<Vertex, EdgeIndex> >
+   : public detail::indexed_vertex_properties<BOOST_CSR_GRAPH_TYPE, VertexProperty,
+                                              Vertex>,
+     public detail::indexed_edge_properties<BOOST_CSR_GRAPH_TYPE, EdgeProperty,
+                                            csr_edge_descriptor<Vertex,
+								EdgeIndex> >
 
 {
-  typedef indexed_vertex_properties<compressed_sparse_row_graph,
-                                    VertexProperty, Vertex>
+  typedef detail::indexed_vertex_properties<compressed_sparse_row_graph,
+					    VertexProperty, Vertex>
     inherited_vertex_properties;
 
-  typedef indexed_edge_properties<BOOST_CSR_GRAPH_TYPE, EdgeProperty,
-                                  csr_edge_descriptor<Vertex, EdgeIndex> >
+  typedef detail::indexed_edge_properties<BOOST_CSR_GRAPH_TYPE, EdgeProperty,
+					  csr_edge_descriptor<Vertex, EdgeIndex> >
     inherited_edge_properties;
 
  public:
