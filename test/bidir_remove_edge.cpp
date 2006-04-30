77e5f0701d9f22ed6c33eecada9a5eb1d7398bea
Dave Abrahams <dave@boostpro.com>
2006-04-30 03:42:14 +0000

    Make it so the test will actually test something in release mode builds.
    [SVN r33872]

--- test/bidir_remove_edge.cpp
+++ test/bidir_remove_edge.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/cstdlib.hpp>
-#include <boost/test/minimal.hpp>
+#include <boost/detail/lightweight_test.hpp>
 
 struct edge_prop {
   int weight;
@@ -25,10 +25,10 @@ test_main(int, char*[])
     edge_prop p = { 42 };
     edge e; bool b;
     tie(e, b) = add_edge(0, 1, p, g);
-    assert( num_edges(g) == 1 );
-    assert( g[e].weight == 42 );
+    BOOST_TEST( num_edges(g) == 1 );
+    BOOST_TEST( g[e].weight == 42 );
     remove_edge(e, g);
-    assert( num_edges(g) == 0 );
+    BOOST_TEST( num_edges(g) == 0 );
   }
   {
     typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS> graph;
@@ -38,9 +38,9 @@ test_main(int, char*[])
 
     edge e; bool b;
     tie(e, b) = add_edge(0, 1, g);
-    assert( num_edges(g) == 1 );
+    BOOST_TEST( num_edges(g) == 1 );
     remove_edge(e, g);
-    assert( num_edges(g) == 0 );
+    BOOST_TEST( num_edges(g) == 0 );
   }
-  return boost::exit_success;
+  return boost::report_errors();
 }
