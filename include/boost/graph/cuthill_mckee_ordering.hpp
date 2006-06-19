826f7a33d8a5ffc651fc0baa73631ffd4b3f157b
Caleb Epstein <boost-bklyn@sneakemail.com>
2006-06-19 14:50:30 +0000

    boost/graph/{king_ordering,cuthill_mckee_ordering}.hpp: Fix for typedef conflict with DS macro on Solaris (merge from RC_1_34_0)
    [SVN r34356]

--- include/boost/graph/cuthill_mckee_ordering.hpp
+++ include/boost/graph/cuthill_mckee_ordering.hpp
@@ -41,9 +41,9 @@ namespace boost {
       void finish_vertex(Vertex, Graph&) {
         using std::sort;
 
-        typedef typename property_traits<DegreeMap>::value_type DS;
+        typedef typename property_traits<DegreeMap>::value_type ds_type;
 
-        typedef indirect_cmp<DegreeMap, std::less<DS> > Compare;
+        typedef indirect_cmp<DegreeMap, std::less<ds_type> > Compare;
         Compare comp(degree);
                 
         sort(Qptr->begin()+index_begin, Qptr->end(), comp);
@@ -74,7 +74,7 @@ namespace boost {
   {
 
     //create queue, visitor...don't forget namespaces!
-    typedef typename property_traits<DegreeMap>::value_type DS;
+    typedef typename property_traits<DegreeMap>::value_type ds_type;
     typedef typename graph_traits<Graph>::vertex_descriptor Vertex;
     typedef typename boost::sparse::sparse_ordering_queue<Vertex> queue;
     typedef typename detail::bfs_rcm_visitor<OutputIterator, queue, DegreeMap> Visitor;
