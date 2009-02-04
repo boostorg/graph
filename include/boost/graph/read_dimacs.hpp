23e75246cbb2878e4f5c3d8c806b02e33d2afab5
Jeremiah Willcock <jewillco@osl.iu.edu>
2009-02-04 19:28:31 +0000

    Fixed qcc errors
    [SVN r51014]

--- include/boost/graph/read_dimacs.hpp
+++ include/boost/graph/read_dimacs.hpp
@@ -18,6 +18,7 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
+#include <cstdlib>
 
 #include <boost/graph/graph_traits.hpp>
 
@@ -134,13 +135,13 @@ int read_dimacs_max_flow(Graph& g,
 
       if (
           /* reading problem line: type of problem, no of nodes, no of arcs */
-          sscanf ( in_line.c_str(), "%*c %3s %ld %ld", pr_type, &n, &m )
+          std::sscanf ( in_line.c_str(), "%*c %3s %ld %ld", pr_type, &n, &m )
           != P_FIELDS
           )
         /*wrong number of parameters in the problem line*/
         { err_no = EN2; goto error; }
 
-      if ( strcmp ( pr_type, PROBLEM_TYPE ) )
+      if ( std::strcmp ( pr_type, PROBLEM_TYPE ) )
         /*wrong problem type*/
         { err_no = EN3; goto error; }
 
@@ -160,7 +161,7 @@ int read_dimacs_max_flow(Graph& g,
         { err_no = EN8; goto error; }
 
       /* reading source  or sink */
-      k = sscanf ( in_line.c_str(),"%*c %ld %c", &i, &nd );
+      k = std::sscanf ( in_line.c_str(),"%*c %ld %c", &i, &nd );
       --i; // index from 0
       if ( k < NODE_FIELDS )
         /* node line is incorrect */
@@ -208,8 +209,8 @@ int read_dimacs_max_flow(Graph& g,
 
       if (
           /* reading an arc description */
-          sscanf ( in_line.c_str(),"%*c %ld %ld %ld",
-                   &tail, &head, &cap )
+          std::sscanf ( in_line.c_str(),"%*c %ld %ld %ld",
+                        &tail, &head, &cap )
           != ARC_FIELDS
           )
         /* arc description is not correct */
@@ -269,10 +270,10 @@ int read_dimacs_max_flow(Graph& g,
   /* ---------------------------------- */
  error:  /* error found reading input */
 
-  printf ( "\nline %ld of input - %s\n",
-           no_lines, err_message[err_no] );
+  std::printf ( "\nline %ld of input - %s\n",
+                no_lines, err_message[err_no] );
 
-  exit (1);
+  std::exit (1);
   return (0); /* to avoid warning */
 }
 /* --------------------   end of parser  -------------------*/
