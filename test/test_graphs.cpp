77d5bf7b802b7142646eeb43e5618766f83225ea
Andrew Sutton <andrew.n.sutton@gmail.com>
2009-02-08 14:25:32 +0000

    Integrating SOC 2007 code
    [SVN r51092]

---
+++ test/test_graphs.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+
+#include <boost/mpl/bool.hpp>
+#include <boost/graph/undirected_graph.hpp>
+#include <boost/graph/directed_graph.hpp>
+
+using namespace std;
+using namespace boost;
+
+struct node
+{
+    node() : n() { }
+    node(int n) : n(n) { }
+
+    bool operator==(node const& x) const    { return n == x.n; }
+    bool operator<(node const& x) const     { return n < x.n; }
+
+    int n;
+};
+
+struct arc
+{
+    arc() : n() { }
+    arc(int n) : n(n) { }
+
+    bool operator==(arc const& x) const     { return n == x.n; }
+
+    int n;
+};
+
+// TODO: Finish implementing this test. Actually, generalize the test so that
+// it works for lots of different graph types.
+
+template <typename Graph>
+void test()
+{
+    typedef typename Graph::vertex_descriptor Vertex;
+    Graph g;
+    BOOST_ASSERT(num_vertices(g) == 0);
+
+}
+
+int main()
+{
+    test< undirected_graph<node, arc> >();
+    test< directed_graph<node, arc> >();
+}
+
