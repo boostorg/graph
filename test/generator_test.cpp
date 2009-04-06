a2843c2352b8ae776878725a50c0668add3aefc4
Jeremiah Willcock <jewillco@osl.iu.edu>
2009-04-06 20:48:42 +0000

    Added copyright and license info
    [SVN r52220]

--- test/generator_test.cpp
+++ test/generator_test.cpp
@@ -1,3 +1,12 @@
+// Copyright 2009 The Trustees of Indiana University.
+
+// Use, modification and distribution is subject to the Boost Software
+// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+//  Authors: Nicholas Edmonds
+//           Andrew Lumsdaine
+
 #include <boost/random.hpp>
 #include <boost/test/minimal.hpp>
 
