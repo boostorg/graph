01b30ee264f4b4711ecd0ecba240f921fea1868d
Jeremiah Willcock <jewillco@osl.iu.edu>
2009-04-11 17:01:34 +0000

    Fixed inspection problems
    [SVN r52336]

--- include/boost/graph/property_iter_range.hpp
+++ include/boost/graph/property_iter_range.hpp
@@ -1,5 +1,5 @@
 
-// (C) Copyright François Faure, iMAGIS-GRAVIR / UJF, 2001.
+// (C) Copyright Francois Faure, iMAGIS-GRAVIR / UJF, 2001.
 //
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
