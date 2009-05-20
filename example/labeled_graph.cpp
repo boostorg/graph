71c8ff92a21afd9d4dcfe2b750593573b94f7e01
Andrew Sutton <andrew.n.sutton@gmail.com>
2009-05-20 14:41:03 +0000

    Added copyright/licence.
    [SVN r53129]

--- example/labeled_graph.cpp
+++ example/labeled_graph.cpp
@@ -1,4 +1,8 @@
-#include "typestr.hpp"
+// (C) Copyright 2009 Andrew Sutton
+//
+// Use, modification and distribution are subject to the
+// Boost Software License, Version 1.0 (See accompanying file
+// LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
 
 #include <iostream>
 #include <string>
