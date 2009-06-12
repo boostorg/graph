84c7f22f1172bd33f2886b35c14b9976d91b01b2
Jeremiah Willcock <jewillco@osl.iu.edu>
2009-06-12 00:42:04 +0000

    Merged r53803 from release branch and rebuilt HTML docs from RST using script from that checkin
    [SVN r53804]

---
+++ doc/BUILD_DOCS.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Copyright (C) 2009 The Trustees of Indiana University.
+# Use, modification and distribution is subject to the Boost Software
+# License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+
+# Authors: Jeremiah Willcock, Andrew Lumsdaine
+
+for i in read_graphml read_graphviz write_graphml; do
+  rst2html.py -gdt --link-stylesheet --traceback --trim-footnote-reference-space --footnote-references=superscript --stylesheet=../../../rst.css $i.rst > $i.html
+done
