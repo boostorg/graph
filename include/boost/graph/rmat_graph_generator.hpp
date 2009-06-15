a5bbe074a6cba47fd94581aa23b307444e975443
Jeremiah Willcock <jewillco@osl.iu.edu>
2009-06-15 16:55:33 +0000

    Added header include to fix error; refs #3134
    [SVN r53938]

--- include/boost/graph/rmat_graph_generator.hpp
+++ include/boost/graph/rmat_graph_generator.hpp
@@ -17,6 +17,7 @@
 #include <map>
 #include <boost/shared_ptr.hpp>
 #include <boost/random/uniform_int.hpp>
+#include <boost/random/uniform_01.hpp>
 #include <boost/graph/graph_traits.hpp>
 #include <boost/type_traits/is_base_and_derived.hpp>
 #include <boost/type_traits/is_same.hpp>
