027f256b5227b563fb914cb7d9d7a0d0c60c0678
Jeremiah Willcock <jewillco@osl.iu.edu>
2009-08-25 20:55:22 +0000

    Added copyright and license info from Michael Hansen
    [SVN r55780]

--- doc/grid_graph_export_svg.sh
+++ doc/grid_graph_export_svg.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#=======================================================================
+# Copyright 2009 Trustees of Indiana University.
+# Authors: Michael Hansen, Andrew Lumsdaine
+#
+# Distributed under the Boost Software License, Version 1.0. (See
+# accompanying file LICENSE_1_0.txt or copy at
+# http:#www.boost.org/LICENSE_1_0.txt)
+#=======================================================================
+
 # Unindexed, unwrapped
 inkscape --export-png grid_graph_unwrapped.png --export-id g3150 --export-id-only grid_graph_unindexed.svg
 
