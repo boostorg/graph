0d4b7e6b7b6160d8f43bb96be686fefda6356199
Jeremiah Willcock <jewillco@osl.iu.edu>
2010-07-05 15:20:19 +0000

    Fixed warning syntax
    [SVN r63655]

--- include/boost/graph/kolmogorov_max_flow.hpp
+++ include/boost/graph/kolmogorov_max_flow.hpp
@@ -32,9 +32,11 @@
 #ifndef BOOST_KOLMOGOROV_MAX_FLOW_HPP
 #define BOOST_KOLMOGOROV_MAX_FLOW_HPP
 
-#warning \
-  The kolmogorov_max_flow.hpp header is deprecated and will be removed \
-  in Boost 1.46. Use boykov_kolmogorov_max_flow.hpp instead.
+#if defined(_MSC_VER) || defined(__BORLANDC__) || defined(__DMC__)
+#  pragma message ("The kolmogorov_max_flow.hpp header is deprecated and will be removed in Boost 1.46. Use boykov_kolmogorov_max_flow.hpp instead.")
+#elif defined(__GNUC__) || defined(__HP_aCC) || defined(__SUNPRO_CC) || defined(__IBMCPP__)
+#  warning "The kolmogorov_max_flow.hpp header is deprecated and will be removed in Boost 1.46. Use boykov_kolmogorov_max_flow.hpp instead."
+#endif
 
 #include <boost/config.hpp>
 #include <cassert>
