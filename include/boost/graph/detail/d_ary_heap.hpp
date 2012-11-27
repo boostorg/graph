74dc17ee946fb74176998f5c79bd342ec9418b7c
Jeremiah Willcock <jewillco@osl.iu.edu>
2012-11-27 22:11:57 +0000

    Added assertions and cast on property map value
    [SVN r81597]

--- include/boost/graph/detail/d_ary_heap.hpp
+++ include/boost/graph/detail/d_ary_heap.hpp
@@ -126,18 +126,21 @@ namespace boost {
     }
 
     Value& top() {
+      assert (!this->empty());
       return data[0];
     }
 
     const Value& top() const {
+      assert (!this->empty());
       return data[0];
     }
 
     void pop() {
+      assert (!this->empty());
       put(index_in_heap, data[0], (size_type)(-1));
       if (data.size() != 1) {
         data[0] = data.back();
-        put(index_in_heap, data[0], 0);
+        put(index_in_heap, data[0], (size_type)(0));
         data.pop_back();
         preserve_heap_property_down();
         verify_heap();
