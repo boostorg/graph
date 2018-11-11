ac16afcbcf743374ac2e6cb81054647275b44e88
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2018-11-12 00:13:27 +1100

    Remove some dead code and indent k_ary_tree_base.

--- include/boost/graph/k-ary_tree.hpp
+++ include/boost/graph/k-ary_tree.hpp
@@ -52,23 +52,6 @@ namespace boost
 
   namespace detail
   {
-    template <std::size_t K, bool Predecessor, typename Vertex>
-    struct k_ary_tree_traits;
-
-    // Directed (aka forward)
-    template <std::size_t K, typename Vertex>
-    struct k_ary_tree_traits<K, false, Vertex>
-    {
-      typedef directed_tag directed_category;
-    };
-
-    // Bidirectional
-    template <std::size_t K, typename Vertex>
-    struct k_ary_tree_traits<K, true, Vertex>
-    {
-      typedef bidirectional_tag directed_category;
-    };
-
     template <typename Tree>
     struct k_ary_tree_forward_node
     {
@@ -77,12 +60,6 @@ namespace boost
       k_ary_tree_forward_node()
       {
         fill(successors, graph_traits<Tree>::null_vertex());
-      }
-
-      template <typename VertexRange>
-      k_ary_tree_forward_node(VertexRange const &successors)
-        : successors(successors)
-      {
       }
 
       array<vertex_descriptor, Tree::k> successors;
@@ -101,143 +78,142 @@ namespace boost
       vertex_descriptor predecessor;
     };
 
-  template <std::size_t K, typename Vertex, typename Node>
-  class k_ary_tree_base
-  {
-  public:
-    typedef Vertex vertex_descriptor;
-
-    BOOST_STATIC_CONSTEXPR vertex_descriptor null_vertex()
+    template <std::size_t K, typename Vertex, typename Node>
+    class k_ary_tree_base
     {
-      return vertex_descriptor(-1);
-    }
+    public:
+      typedef Vertex vertex_descriptor;
 
-    typedef std::pair<vertex_descriptor, vertex_descriptor> edge_descriptor;
-    typedef disallow_parallel_edge_tag edge_parallel_category;
-    typedef std::size_t degree_size_type;
-    BOOST_STATIC_CONSTEXPR std::size_t k = K;
-
-  protected:
-    typedef typename array<vertex_descriptor, k>::const_iterator vertex_iterator;
-
-    struct make_out_edge_descriptor
-    {
-      make_out_edge_descriptor(vertex_descriptor source) : source(source) {}
-
-      edge_descriptor operator()(vertex_descriptor target) const
+      BOOST_STATIC_CONSTEXPR vertex_descriptor null_vertex()
       {
-        return edge_descriptor(source, target);
+        return vertex_descriptor(-1);
       }
 
-      vertex_descriptor source;
+      typedef std::pair<vertex_descriptor, vertex_descriptor> edge_descriptor;
+      typedef disallow_parallel_edge_tag edge_parallel_category;
+      typedef std::size_t degree_size_type;
+      BOOST_STATIC_CONSTEXPR std::size_t k = K;
+
+    protected:
+      typedef typename array<vertex_descriptor, k>::const_iterator vertex_iterator;
+
+      struct make_out_edge_descriptor
+      {
+        make_out_edge_descriptor(vertex_descriptor source) : source(source) {}
+
+        edge_descriptor operator()(vertex_descriptor target) const
+        {
+          return edge_descriptor(source, target);
+        }
+
+        vertex_descriptor source;
+      };
+
+    public:
+      typedef transform_iterator<make_out_edge_descriptor, vertex_descriptor const *, edge_descriptor> out_edge_iterator;
+
+      k_ary_tree_base()
+      {
+        free_list.push_back(0);
+      }
+
+      // *** IncidenceGraph ***
+
+      friend
+      vertex_descriptor source(edge_descriptor e, k_ary_tree_base const &)
+      {
+        return e.first;
+      }
+
+      friend
+      vertex_descriptor target(edge_descriptor e, k_ary_tree_base const &)
+      {
+        return e.second;
+      }
+
+      friend
+      std::pair<out_edge_iterator, out_edge_iterator>
+      out_edges(vertex_descriptor u, k_ary_tree_base const &g)
+      {
+        return std::make_pair(out_edge_iterator(begin(g.nodes[u].successors),
+                                                make_out_edge_descriptor(u)),
+                              out_edge_iterator(end(g.nodes[u].successors),
+                                                make_out_edge_descriptor(u)));
+      }
+
+      friend
+      degree_size_type out_degree(vertex_descriptor v, k_ary_tree_base const &g)
+      {
+        return k - count(g.nodes[v].successors, null_vertex());
+      }
+
+      // *** VertexListGraph interface ***
+
+      friend
+      std::size_t num_vertices(k_ary_tree_base const &g)
+      {
+        return g.num_vertices();
+      }
+
+      // *** MutableGraph interface ***
+
+      friend
+      vertex_descriptor add_vertex(k_ary_tree_base &g)
+      {
+        return g.add_vertex();
+      }
+
+      friend
+      void remove_vertex(vertex_descriptor u, k_ary_tree_base &g)
+      {
+        g.remove_vertex(u);
+      }
+
+    protected:
+      std::size_t num_vertices() const
+      {
+        BOOST_ASSERT(!free_list.empty());
+        // Careful arithmetic so no underflow.
+        return nodes.size() - (free_list.size() - 1);
+      }
+
+      vertex_descriptor add_vertex()
+      {
+        BOOST_ASSERT(!free_list.empty());
+
+        nodes.resize(std::max(vertex_descriptor(nodes.size()), free_list.back() + 1));
+        vertex_descriptor const result = free_list.back();
+        if (free_list.size() == 1)
+          free_list.back() = nodes.size();
+        else
+          free_list.pop_back();
+        return result;
+      }
+
+      void remove_vertex(vertex_descriptor u)
+      {
+        BOOST_ASSERT(num_vertices() > 0);
+        BOOST_ASSERT(!free_list.empty());
+        BOOST_ASSERT(find(free_list, u) == end(free_list));
+        // TODO: Assertions that u is disconnected.
+
+        if (u == nodes.size() - 1)
+        {
+          // happens to be the last node
+          nodes.pop_back();
+          free_list[0]--;
+        }
+        else
+        {
+          free_list.push_back(u);
+        }
+
+        BOOST_ASSERT(find(free_list, u) != end(free_list));
+      }
+
+      std::vector<Node> nodes;
+      std::vector<vertex_descriptor> free_list; // Keeps track of holes in storage.
     };
-
-  public:
-    typedef transform_iterator<make_out_edge_descriptor, vertex_descriptor const *, edge_descriptor> out_edge_iterator;
-
-    k_ary_tree_base()
-    {
-      free_list.push_back(0);
-    }
-
-    // *** IncidenceGraph ***
-
-    friend
-    vertex_descriptor source(edge_descriptor e, k_ary_tree_base const &)
-    {
-      return e.first;
-    }
-
-    friend
-    vertex_descriptor target(edge_descriptor e, k_ary_tree_base const &)
-    {
-      return e.second;
-    }
-
-    friend
-    std::pair<out_edge_iterator, out_edge_iterator>
-    out_edges(vertex_descriptor u, k_ary_tree_base const &g)
-    {
-      return std::make_pair(out_edge_iterator(begin(g.nodes[u].successors),
-                                              make_out_edge_descriptor(u)),
-                            out_edge_iterator(end(g.nodes[u].successors),
-                                              make_out_edge_descriptor(u)));
-    }
-
-    friend
-    degree_size_type out_degree(vertex_descriptor v, k_ary_tree_base const &g)
-    {
-      return k - count(g.nodes[v].successors, null_vertex());
-    }
-
-    // *** VertexListGraph interface ***
-
-    friend
-    std::size_t num_vertices(k_ary_tree_base const &g)
-    {
-      return g.num_vertices();
-    }
-
-    // *** MutableGraph interface ***
-
-    friend
-    vertex_descriptor add_vertex(k_ary_tree_base &g)
-    {
-      return g.add_vertex();
-    }
-
-    friend
-    void remove_vertex(vertex_descriptor u, k_ary_tree_base &g)
-    {
-      g.remove_vertex(u);
-    }
-
-  protected:
-    std::size_t num_vertices() const
-    {
-      BOOST_ASSERT(!free_list.empty());
-      // Careful arithmetic so no underflow.
-      return nodes.size() - (free_list.size() - 1);
-    }
-
-    vertex_descriptor add_vertex()
-    {
-      BOOST_ASSERT(!free_list.empty());
-
-      nodes.resize(std::max(vertex_descriptor(nodes.size()), free_list.back() + 1));
-      vertex_descriptor const result = free_list.back();
-      if (free_list.size() == 1)
-        free_list.back() = nodes.size();
-      else
-        free_list.pop_back();
-      return result;
-    }
-
-    void remove_vertex(vertex_descriptor u)
-    {
-      BOOST_ASSERT(num_vertices() > 0);
-      BOOST_ASSERT(!free_list.empty());
-      BOOST_ASSERT(find(free_list, u) == end(free_list));
-      // TODO: Assertions that u is disconnected.
-
-      if (u == nodes.size() - 1)
-      {
-        // happens to be the last node
-        nodes.pop_back();
-        free_list[0]--;
-      }
-      else
-      {
-        free_list.push_back(u);
-      }
-
-      BOOST_ASSERT(find(free_list, u) != end(free_list));
-    }
-
-    std::vector<Node> nodes;
-    std::vector<vertex_descriptor> free_list; // Keeps track of holes in storage.
-  };
-
   }
 
   template <std::size_t K, bool Predecessor, typename Vertex = std::size_t>
