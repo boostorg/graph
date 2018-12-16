977560b9179af2f2cad526b68877451d00d3f943
jzmaddock <john@johnmaddock.co.uk>
2018-12-16 10:10:04 +0000

    Remove adj_list_test.cpp as it could never be portably run, replace with updated version of graph_type.hpp which tests all the same permutations that adj_list_test.cpp was intended to handle.  Update Jamfile.v2 to run all the permutations.

--- test/graph_type.hpp
+++ test/graph_type.hpp
@@ -7,7 +7,42 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 //=======================================================================
 
+//
+// The following test permutations are extracted from the old adj_list_test.cpp
+// which generated code on the fly, but was never run as part of the regular tests.
+//
+#if  TEST == 1
+#define TEST_TYPE vecS
+#define DIRECTED_TYPE bidirectionalS
+#elif TEST == 2
+#define TEST_TYPE vecS
+#define DIRECTED_TYPE directedS
+#elif TEST == 3
+#define TEST_TYPE vecS
+#define DIRECTED_TYPE undirectedS
+#elif  TEST == 4
+#define TEST_TYPE listS
+#define DIRECTED_TYPE bidirectionalS
+#elif TEST == 5
+#define TEST_TYPE listS
+#define DIRECTED_TYPE directedS
+#elif TEST == 6
+#define TEST_TYPE listS
+#define DIRECTED_TYPE undirectedS
+#elif  TEST == 7
+#define TEST_TYPE setS
+#define DIRECTED_TYPE bidirectionalS
+#elif TEST == 8
+#define TEST_TYPE setS
+#define DIRECTED_TYPE directedS
+#elif TEST == 9
+#define TEST_TYPE setS
+#define DIRECTED_TYPE undirectedS
+#else
+#error "No test combination specified - define macro TEST to the value 1 - 9."
+#endif
+
 #include <boost/graph/adjacency_list.hpp>
-typedef boost::adjacency_list<boost::setS, boost::setS, boost::undirectedS, boost::property<vertex_id_t, std::size_t>, boost::property<edge_id_t, std::size_t> > Graph;
+typedef boost::adjacency_list<boost::TEST_TYPE, boost::TEST_TYPE, boost::DIRECTED_TYPE, boost::property<vertex_id_t, std::size_t>, boost::property<edge_id_t, std::size_t> > Graph;
 typedef boost::property<vertex_id_t, std::size_t> VertexId;
 typedef boost::property<edge_id_t, std::size_t> EdgeID;
