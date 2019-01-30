c69d25f4c5903bd7fedae7e06ece0d0e8132826c
David Einstein <deinst@gmail.com>
2019-01-29 21:51:59 -0500

    Updated kruskal-telephone
    This was pretty much a complete rewrite.

--- example/kruskal-telephone.cpp
+++ example/kruskal-telephone.cpp
@@ -5,15 +5,6 @@
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //=======================================================================
-
-/*
-   IMPORTANT!!!
-   ~~~~~~~~~~~~
-   This example uses interfaces that have been deprecated and removed from Boost.Grpah.
-   Someone needs to update it, as it does NOT compile.
-*/
-
-
 #include <boost/config.hpp>
 #include <iostream>
 #include <fstream>
@@ -21,6 +12,68 @@
 #include <boost/graph/graphviz.hpp>
 #include <boost/graph/kruskal_min_spanning_tree.hpp>
 
+using namespace boost;
+
+struct graph_writer{
+  void operator()(std::ostream & os) {
+    os << "size=\"5,5.3\"" << std::endl;
+    os << "ratio=\"fill\"" << std::endl;
+    os << "edge[color=\"gray\", style=\"bold\"]" << std::endl;
+  }
+};
+
+template <typename Edge, typename Label, typename Weight >
+struct edge_writer{
+  edge_writer(Label _label, Weight _weight, std::set< Edge >& _tree) :
+    label(_label), weight(_weight), tree(_tree) {}
+  void operator()(std::ostream& os, const Edge& edge) const {
+    os << "[label=" << escape_dot_string(get(label, edge));
+    os << ", weight=" << escape_dot_string(get(weight, edge));
+    if (tree.find(edge) != tree.end()) {
+      os << ", color=\"black\"";
+    }
+    os << "]";
+  }
+  Label label; 
+  Weight weight;
+  std::set< Edge > tree;
+};
+
+int
+main(int argc, char *argv[]) {
+  std::ifstream ifs(argc >= 2 ? argv[1] : "figs/telephone-network.dot");
+  typedef property< vertex_name_t, std::string > VertexProperty;
+  typedef property< edge_weight_t, int, property< edge_weight2_t, int > > EdgeProperty;
+  typedef adjacency_list< vecS, vecS, undirectedS, VertexProperty, EdgeProperty > Graph;
+  Graph g;
+
+  dynamic_properties dp(ignore_other_properties);
+  dp.property("node_id", get(vertex_name, g));
+  // using the label field for the weight is odd, but this is what was in the original example.
+  dp.property("label", get(edge_weight, g));
+  dp.property("weight", get(edge_weight2, g));
+
+  read_graphviz(ifs, g, dp);
+  typedef property_map <Graph, edge_weight_t>::type LabelMap;
+  typedef property_map <Graph, edge_weight2_t>::type WeightMap;
+  typedef graph_traits<Graph>::edge_descriptor edge_t;
+  std::set<edge_t> edge_set;
+  kruskal_minimum_spanning_tree(g, std::inserter(edge_set, edge_set.end()));
+  LabelMap labels = get(edge_weight_t(), g);
+  int total_weight = 0;
+  for (std::set<edge_t>::iterator it = edge_set.begin(); it != edge_set.end(); ++it) {
+    total_weight += get(labels, *it);
+  }
+  std::cout << "total weight: " << total_weight << std::endl;
+  WeightMap weights = get(edge_weight2_t(), g);
+  graph_writer gw;
+  edge_writer<edge_t, LabelMap, WeightMap >
+    e_write(labels, weights, edge_set);
+  std::ofstream ofs("figs/telephone-mst-kruskal.dot");
+  write_graphviz(ofs, g, default_writer(), e_write, gw, get(vertex_name_t(), g));
+}
+
+#if 0 
 int
 main()
 {
@@ -64,3 +117,4 @@ main()
 
   return EXIT_SUCCESS;
 }
+#endif
