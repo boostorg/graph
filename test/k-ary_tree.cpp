5986ee0d3ee5d400251d9f9678ef959785d3f5fa
Jeremy W. Murphy <jeremy.william.murphy@gmail.com>
2019-05-02 00:54:25 -0700

    Use current Boost.Test headers, shared library linking.
    Also involves using C++14.

--- test/k-ary_tree.cpp
+++ test/k-ary_tree.cpp
@@ -10,11 +10,14 @@
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/graph_concepts.hpp>
 
-#include <boost/test/minimal.hpp>
-#include <boost/array.hpp>
+#define BOOST_TEST_MODULE k-ary tree
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
 #include <boost/range.hpp>
 #include <boost/range/algorithm.hpp>
 
+#include <array>
+#include <tuple>
 #include <utility>
 
 template <typename Graph>
@@ -32,104 +35,98 @@ void create_full_tree(Graph &tree,
   }
 }
 
+using tree_types = std::tuple<boost::forward_binary_tree,
+                              boost::bidirectional_binary_tree>;
 
-void empty_forward_binary_tree()
+BOOST_AUTO_TEST_CASE_TEMPLATE(empty_tree, BinaryTree, tree_types)
 {
-  boost::forward_binary_tree tree;
-  BOOST_CHECK(num_vertices(tree) == 0);
+  BinaryTree tree;
+  BOOST_TEST(num_vertices(tree) == 0);
 }
 
-void empty_bidirectional_binary_tree()
-{
-  boost::bidirectional_binary_tree tree;
-  BOOST_CHECK(num_vertices(tree) == 0);
-}
 
-template <typename Tree>
-void push_pop_binary_tree()
+BOOST_AUTO_TEST_CASE_TEMPLATE(push_pop, BinaryTree, tree_types)
 {
-  Tree tree;
-  typedef typename boost::graph_traits<Tree>::vertex_descriptor vertex_descriptor;
+  BinaryTree tree;
+  typedef typename boost::graph_traits<BinaryTree>::vertex_descriptor vertex_descriptor;
   vertex_descriptor u = add_vertex(tree);
-  BOOST_ASSERT(num_vertices(tree) == 1);
+  BOOST_TEST(num_vertices(tree) == 1);
   remove_vertex(u, tree);
-  BOOST_ASSERT(num_vertices(tree) == 0);
+  BOOST_TEST(num_vertices(tree) == 0);
 
   std::vector<vertex_descriptor> added;
   added.push_back(add_vertex(tree));
   added.push_back(add_vertex(tree));
   added.push_back(add_vertex(tree));
-  BOOST_ASSERT(num_vertices(tree) == 3);
+  BOOST_TEST(num_vertices(tree) == 3);
   remove_vertex(added.back(), tree);
   added.pop_back();
   remove_vertex(added.back(), tree);
   added.pop_back();
   remove_vertex(added.back(), tree);
   added.pop_back();
-  BOOST_ASSERT(num_vertices(tree) == 0);
+  BOOST_TEST(num_vertices(tree) == 0);
 }
 
 
-template <typename Tree>
-void insert_remove_randomly()
+BOOST_AUTO_TEST_CASE_TEMPLATE(insert_remove_randomly, BinaryTree, tree_types)
 {
-  Tree tree;
-  typedef typename boost::graph_traits<Tree>::vertex_descriptor vertex_descriptor;
+  BinaryTree tree;
+  typedef typename boost::graph_traits<BinaryTree>::vertex_descriptor vertex_descriptor;
 
   std::vector<vertex_descriptor> added;
   added.push_back(add_vertex(tree));
   added.push_back(add_vertex(tree));
   added.push_back(add_vertex(tree));
-  BOOST_ASSERT(num_vertices(tree) == 3);
+  BOOST_TEST(num_vertices(tree) == 3);
   remove_vertex(added[0], tree);
-  BOOST_ASSERT(num_vertices(tree) == 2);
+  BOOST_TEST(num_vertices(tree) == 2);
   remove_vertex(added[1], tree);
-  BOOST_ASSERT(num_vertices(tree) == 1);
+  BOOST_TEST(num_vertices(tree) == 1);
   added[0] = add_vertex(tree);
-  BOOST_ASSERT(num_vertices(tree) == 2);
+  BOOST_TEST(num_vertices(tree) == 2);
   added[1] = add_vertex(tree);
-  BOOST_ASSERT(num_vertices(tree) == 3);
+  BOOST_TEST(num_vertices(tree) == 3);
   add_edge(added[0], added[1], tree);
   add_edge(4, 5, tree);
 }
 
-template <typename Tree>
-void incidence_graph()
+BOOST_AUTO_TEST_CASE_TEMPLATE(incidence_graph, BinaryTree, tree_types)
 {
-  Tree tree;
-  typedef typename boost::graph_traits<Tree>::vertex_descriptor vertex_descriptor;
+  BinaryTree tree;
+  typedef typename boost::graph_traits<BinaryTree>::vertex_descriptor vertex_descriptor;
 
   vertex_descriptor u = add_vertex(tree);
-  BOOST_CHECK(boost::distance(out_edges(u, tree)) == 0);
-  BOOST_CHECK(out_degree(u, tree) == 0);
+  BOOST_TEST(boost::distance(out_edges(u, tree)) == 0);
+  BOOST_TEST(out_degree(u, tree) == 0);
   add_edge(u, u + 1, tree);
-  BOOST_CHECK(boost::distance(out_edges(u, tree)) == 1);
-  BOOST_CHECK(out_degree(u, tree) == 1);
+  BOOST_TEST(boost::distance(out_edges(u, tree)) == 1);
+  BOOST_TEST(out_degree(u, tree) == 1);
 }
 
-void bidirectional_graph()
+BOOST_AUTO_TEST_CASE(bidirectional)
 {
   boost::bidirectional_binary_tree tree;
   create_full_tree(tree, 5);
-  BOOST_CHECK(boost::distance(in_edges(0, tree)) == 0);
-  BOOST_CHECK(in_degree(0, tree) == 0);
-  BOOST_CHECK(boost::distance(in_edges(1, tree)) == 1);
-  BOOST_CHECK(in_degree(1, tree) == 1);
-  BOOST_CHECK(boost::distance(in_edges(2, tree)) == 1);
-  BOOST_CHECK(in_degree(2, tree) == 1);
-  BOOST_CHECK(root(0, tree) == 0);
-  BOOST_CHECK(root(1, tree) == 0);
-  BOOST_CHECK(root(2, tree) == 0);
-  BOOST_CHECK(root(3, tree) == 0);
-  BOOST_CHECK(root(4, tree) == 0);
+  BOOST_TEST(boost::distance(in_edges(0, tree)) == 0);
+  BOOST_TEST(in_degree(0, tree) == 0);
+  BOOST_TEST(boost::distance(in_edges(1, tree)) == 1);
+  BOOST_TEST(in_degree(1, tree) == 1);
+  BOOST_TEST(boost::distance(in_edges(2, tree)) == 1);
+  BOOST_TEST(in_degree(2, tree) == 1);
+  BOOST_TEST(root(0, tree) == 0);
+  BOOST_TEST(root(1, tree) == 0);
+  BOOST_TEST(root(2, tree) == 0);
+  BOOST_TEST(root(3, tree) == 0);
+  BOOST_TEST(root(4, tree) == 0);
   // detach the (1(3, 4)) subtree.
   remove_edge(0, 1, tree);
-  BOOST_CHECK(!has_left_successor(0, tree));
-  BOOST_CHECK(root(0, tree) == 0);
-  BOOST_CHECK(root(1, tree) == 1);
-  BOOST_CHECK(root(2, tree) == 0);
-  BOOST_CHECK(root(3, tree) == 1);
-  BOOST_CHECK(root(4, tree) == 1);
+  BOOST_TEST(!has_left_successor(0, tree));
+  BOOST_TEST(root(0, tree) == 0);
+  BOOST_TEST(root(1, tree) == 1);
+  BOOST_TEST(root(2, tree) == 0);
+  BOOST_TEST(root(3, tree) == 1);
+  BOOST_TEST(root(4, tree) == 1);
 }
 
 template <typename Order, typename Vertex>
@@ -143,19 +140,16 @@ struct tree_visitor
   std::vector< std::pair<Order, Vertex> > visited;
 };
 
-
-template <bool Predecessor>
-void depth_first_search()
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_depth_first_search, BinaryTree, tree_types)
 {
-  typedef boost::k_ary_tree<2, Predecessor> Tree;
-  boost::k_ary_tree<2, Predecessor> tree;
-  typedef typename boost::graph_traits<Tree>::vertex_descriptor vertex_descriptor;
+  BinaryTree tree;
+  typedef typename boost::graph_traits<BinaryTree>::vertex_descriptor vertex_descriptor;
 
   create_full_tree(tree, 3);
 
   std::vector<boost::default_color_type> color;
   typedef std::pair<boost::order::visit, vertex_descriptor> visiting;
-  boost::array< visiting, 9> const expected_seq =
+  std::array< visiting, 9> const expected_seq =
   {{
     std::make_pair(boost::order::pre, 0),
     std::make_pair(boost::order::pre, 1),
@@ -169,50 +163,48 @@ void depth_first_search()
   }};
   tree_visitor<boost::order::visit, vertex_descriptor> visitor;
   depth_first_visit(tree, 0, visitor, color);
-  BOOST_CHECK(boost::equal(visitor.visited, expected_seq));
+  BOOST_TEST(boost::equal(visitor.visited, expected_seq));
 }
 
 
-void mutable_bidirectional()
+BOOST_AUTO_TEST_CASE(mutable_bidirectional)
 {
-  typedef boost::k_ary_tree<2, true> Tree;
-  Tree tree;
-  typedef boost::graph_traits<Tree>::vertex_descriptor vertex_descriptor;
+  typedef boost::k_ary_tree<2, true> BinaryTree;
+  BinaryTree tree;
+  typedef boost::graph_traits<BinaryTree>::vertex_descriptor vertex_descriptor;
 
   create_full_tree(tree, 3);
 
-  BOOST_CHECK(predecessor(vertex_descriptor(1), tree) == 0);
-  BOOST_CHECK(predecessor(vertex_descriptor(2), tree) == 0);
+  BOOST_TEST(predecessor(vertex_descriptor(1), tree) == 0);
+  BOOST_TEST(predecessor(vertex_descriptor(2), tree) == 0);
 
-  BOOST_CHECK(has_left_successor(vertex_descriptor(0), tree));
-  BOOST_CHECK(has_right_successor(vertex_descriptor(0), tree));
+  BOOST_TEST(has_left_successor(vertex_descriptor(0), tree));
+  BOOST_TEST(has_right_successor(vertex_descriptor(0), tree));
 
   clear_vertex(vertex_descriptor(0), tree);
 
-  BOOST_CHECK(num_vertices(tree) == 3);
+  BOOST_TEST(num_vertices(tree) == 3);
 
-  vertex_descriptor null = boost::graph_traits<Tree>::null_vertex();
+  vertex_descriptor null = boost::graph_traits<BinaryTree>::null_vertex();
 
-  BOOST_CHECK(predecessor(vertex_descriptor(1), tree) == null);
-  BOOST_CHECK(predecessor(vertex_descriptor(2), tree) == null);
+  BOOST_TEST(predecessor(vertex_descriptor(1), tree) == null);
+  BOOST_TEST(predecessor(vertex_descriptor(2), tree) == null);
 
-  BOOST_CHECK(!has_left_successor(vertex_descriptor(0), tree));
-  BOOST_CHECK(!has_right_successor(vertex_descriptor(0), tree));
+  BOOST_TEST(!has_left_successor(vertex_descriptor(0), tree));
+  BOOST_TEST(!has_right_successor(vertex_descriptor(0), tree));
 
   // Remove after clearing.
   remove_vertex(vertex_descriptor(0), tree);
 
-  BOOST_CHECK(num_vertices(tree) == 2);
+  BOOST_TEST(num_vertices(tree) == 2);
 
 }
 
 
-template <bool Predecessor>
-void binary_tree()
+BOOST_AUTO_TEST_CASE_TEMPLATE(Mutable, BinaryTree, tree_types)
 {
-  typedef boost::k_ary_tree<2, Predecessor> Tree;
-  boost::k_ary_tree<2, Predecessor> tree;
-  typedef typename boost::graph_traits<Tree>::vertex_descriptor vertex_descriptor;
+  BinaryTree tree;
+  typedef typename boost::graph_traits<BinaryTree>::vertex_descriptor vertex_descriptor;
 
   std::vector<vertex_descriptor> added;
 
@@ -221,72 +213,54 @@ void binary_tree()
   added.push_back(add_vertex(tree));
   add_edge(added[0], added[1], tree);
   add_edge(added[0], added[2], tree);
-  BOOST_CHECK(has_left_successor(added[0], tree));
-  BOOST_CHECK(has_right_successor(added[0], tree));
-  BOOST_CHECK(!has_left_successor(added[1], tree));
-  BOOST_CHECK(!has_right_successor(added[1], tree));
-  BOOST_CHECK(!has_left_successor(added[2], tree));
-  BOOST_CHECK(!has_right_successor(added[2], tree));
+  BOOST_TEST(has_left_successor(added[0], tree));
+  BOOST_TEST(has_right_successor(added[0], tree));
+  BOOST_TEST(!has_left_successor(added[1], tree));
+  BOOST_TEST(!has_right_successor(added[1], tree));
+  BOOST_TEST(!has_left_successor(added[2], tree));
+  BOOST_TEST(!has_right_successor(added[2], tree));
 
-  BOOST_CHECK(isomorphism(tree, tree));
+  BOOST_TEST(isomorphism(tree, tree));
 
   remove_edge(added[0], added[1], tree);
   remove_edge(added[0], added[2], tree);
-  BOOST_CHECK(!has_left_successor(added[0], tree));
-  BOOST_CHECK(!has_right_successor(added[0], tree));
+  BOOST_TEST(!has_left_successor(added[0], tree));
+  BOOST_TEST(!has_right_successor(added[0], tree));
 }
 
 
-template <bool Predecessor>
-void VertexListGraph_test()
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_isomorphism, BinaryTree, tree_types)
 {
-  typedef boost::k_ary_tree<2, Predecessor> Tree;
-  Tree tree;
-  typedef typename boost::graph_traits<Tree>::vertex_descriptor vertex_descriptor;
+  BinaryTree tree0, tree1;
+  BOOST_TEST(isomorphism(tree0, tree1));
+  create_full_tree(tree0, 15);
+  create_full_tree(tree1, 15);
+  BOOST_TEST(isomorphism(tree0, tree1));
+}
+
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(VertexListGraph, BinaryTree, tree_types)
+{
+  BinaryTree tree;
+  typedef typename boost::graph_traits<BinaryTree>::vertex_descriptor vertex_descriptor;
 
   create_full_tree(tree, 7);
 
-  boost::array<vertex_descriptor, 7> actual,
-                                      expected = {{3, 1, 4, 0, 5, 2, 6}};
-  copy(vertices(tree), boost::begin(actual));
-  BOOST_CHECK(actual == expected);
+  std::array<vertex_descriptor, 7> actual,
+                                   expected = {{3, 1, 4, 0, 5, 2, 6}};
+  copy(vertices(tree), begin(actual));
+  BOOST_TEST(actual == expected);
 }
 
-int test_main(int, char*[])
-{
-  using namespace boost::concepts;
-  using boost::forward_binary_tree;
-  using boost::bidirectional_binary_tree;
+using namespace boost::concepts;
+using boost::forward_binary_tree;
+using boost::bidirectional_binary_tree;
 
-  BOOST_CONCEPT_ASSERT((IncidenceGraph<forward_binary_tree>));
-  BOOST_CONCEPT_ASSERT((BidirectionalGraph<bidirectional_binary_tree>));
-  BOOST_CONCEPT_ASSERT((MutableGraph<bidirectional_binary_tree>));
-  BOOST_CONCEPT_ASSERT((MutableGraph<forward_binary_tree>));
-  BOOST_CONCEPT_ASSERT((VertexListGraph<forward_binary_tree>));
-  BOOST_CONCEPT_ASSERT((VertexListGraph<bidirectional_binary_tree>));
-  // BOOST_CONCEPT_ASSERT((EdgeListGraph<forward_binary_tree>));
-  // BOOST_CONCEPT_ASSERT((EdgeListGraph<bidirectional_binary_tree>));
-
-  empty_forward_binary_tree();
-  empty_bidirectional_binary_tree();
-  push_pop_binary_tree<forward_binary_tree>();
-  push_pop_binary_tree<bidirectional_binary_tree>();
-  insert_remove_randomly<forward_binary_tree>();
-  insert_remove_randomly<bidirectional_binary_tree>();
-  incidence_graph<forward_binary_tree>();
-  incidence_graph<bidirectional_binary_tree>();
-  bidirectional_graph();
-
-  binary_tree<0>();
-  binary_tree<1>();
-
-  depth_first_search<0>();
-  depth_first_search<1>();
-
-  mutable_bidirectional();
-
-  VertexListGraph_test<0>();
-  VertexListGraph_test<1>();
-
-  return 0;
-}
+BOOST_CONCEPT_ASSERT((IncidenceGraphConcept<forward_binary_tree>));
+BOOST_CONCEPT_ASSERT((BidirectionalGraphConcept<bidirectional_binary_tree>));
+BOOST_CONCEPT_ASSERT((MutableGraphConcept<bidirectional_binary_tree>));
+BOOST_CONCEPT_ASSERT((MutableGraphConcept<forward_binary_tree>));
+BOOST_CONCEPT_ASSERT((VertexListGraphConcept<forward_binary_tree>));
+BOOST_CONCEPT_ASSERT((VertexListGraphConcept<bidirectional_binary_tree>));
+// BOOST_CONCEPT_ASSERT((EdgeListGraph<forward_binary_tree>));
+// BOOST_CONCEPT_ASSERT((EdgeListGraph<bidirectional_binary_tree>));
