972280e23207cb4d70fec09e611eef88824d7c10
Josh Marshall <joshua.r.marshall.1991@gmail.com>
2019-05-11 00:15:48 -0400

    First try at format checking.

---
+++ utility/ci/travis/script.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash -ue
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+
+  find -0 nanodbc test example -type f -name '*.h' -o -name '*.cpp' \
+  | xargs -I{} -P "$(nproc)" clang-format -i -style=file {}
+
+  dirty=$(git ls-files --modified)
+
+  if [[ $dirty ]]; then
+    echo "The following files have been modified:"
+    echo "$dirty"
+
+    exit 1
+  fi
+fi
