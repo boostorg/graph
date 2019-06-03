fbbb5ffac571b23661d4045405584bd790b5b252
Josh Marshall <jrmarsha@mtu.edu>
2019-06-03 19:54:26 -0400

    Experimenting with that path for b2.  Not sure why it doesn't work on build.sh line 8.

--- utility/build.sh
+++ utility/build.sh
@@ -5,4 +5,4 @@ echo "using $TOOLSET : : $COMPILER ;" > ~/user-config.jam
 IFS=','
 for CXXLOCAL in $CXXSTD; do  (cd libs/config/test && ../../../b2 config_info_travis_install toolset=$TOOLSET cxxstd=$CXXLOCAL && ./config_info_travis && rm ./config_info_travis)  done
 unset IFS
-./b2 -j3 libs/$SELF/test toolset=$TOOLSET cxxstd=$CXXSTD
+$BOOST_ROOT/b2 -j3 libs/$SELF/test toolset=$TOOLSET cxxstd=$CXXSTD
