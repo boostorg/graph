56da4b3cc6f375f74a938f7c3d7489b15afb0c66
Josh Marshall <jrmarsha@mtu.edu>
2019-08-11 12:20:27 -0400

    Jiggling the cppcheck job.  Looks like quite a lot got built up and it is quite fragile.

--- utility/ci/travis/cppcheck.sh
+++ utility/ci/travis/cppcheck.sh
@@ -13,21 +13,6 @@ set -exv
 
 . $(dirname "${BASH_SOURCE[0]}")/../enforce_b2_env.sh || exit 1
 
-# Travis comes with older cppcheck, so...
-if [ -z "$CPPCHKVER" ]; then
-    CPPCHKVER=1.87
-fi
-
-pushd ~
-wget https://github.com/danmar/cppcheck/archive/$CPPCHKVER.tar.gz
-tar xzf $CPPCHKVER.tar.gz
-mkdir cppcheck-build
-cd cppcheck-build
-# the next line quiets the build substantially:
-sed -i 's/-Winline/-Wno-inline/g' ../cppcheck-$CPPCHKVER/cmake/compileroptions.cmake
-cmake ../cppcheck-$CPPCHKVER -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=~/cppcheck
-make -j3 install
-popd
 
 STANDARDS=
 while IFS=',' read -ra ADDR; do
@@ -37,7 +22,5 @@ while IFS=',' read -ra ADDR; do
     done
 done <<< "$B2_CXXSTD"
 
-~/cppcheck/bin/cppcheck -I${BOOST_ROOT} ${STANDARDS} --enable=all --error-exitcode=1 \
-     --force --check-config --suppress=*:boost/preprocessor/tuple/size.hpp \
-     -UBOOST_USER_CONFIG -UBOOST_COMPILER_CONFIG -UBOOST_STDLIB_CONFIG -UBOOST_PLATFORM_CONFIG \
-     ${BOOST_ROOT}/libs/$SELF 2>&1 | grep -v 'Cppcheck does not need standard library headers'
+cppcheck -I. ${STANDARDS} --enable=all --error-exitcode=1 \
+     --force --check-config . || exit 1
