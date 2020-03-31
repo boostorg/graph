14c2bf17b49e6f01c7a38f71eff2b0644660cb70
Edward Diener <eldlistmailingz@tropicsoft.com>
2020-03-31 08:36:03 -0400

    Change __BORLANDC__ to BOOST_BORLANDC, which is defined in Boost config for the Embarcadero non-clang-based compilers.

--- include/boost/graph/edmunds_karp_max_flow.hpp
+++ include/boost/graph/edmunds_karp_max_flow.hpp
@@ -8,7 +8,7 @@
 #ifndef BOOST_DEPRECATED_INCLUDE_EDMONDS_KARP_MAX_FLOW_HPP
 #define BOOST_DEPRECATED_INCLUDE_EDMONDS_KARP_MAX_FLOW_HPP
 
-#if defined(_MSC_VER) || defined(__BORLANDC__) || defined(__DMC__)
+#if defined(_MSC_VER) || defined(__BORLANDC__) && !defined(__clang__) || defined(__DMC__)
 #pragma message( \
     "Warning: This header is deprecated. Please use: boost/graph/edmonds_karp_max_flow.hpp")
 #elif defined(__GNUC__) || defined(__HP_aCC) || defined(__SUNPRO_CC) \
