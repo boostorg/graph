e5ccf7977e5e232fd96f72d75f9e9d5a0e3a5afd
vslashg <gfalcon@google.com>
2023-09-08 14:11:07 -0400

    Add a missing include to topology.hpp
    boost::shared_ptr is used in this file without the corresponding include, so currently this file does not compile on its own.

--- include/boost/graph/topology.hpp
+++ include/boost/graph/topology.hpp
@@ -18,6 +18,7 @@
 #include <boost/algorithm/minmax.hpp>
 #include <boost/config.hpp> // For BOOST_STATIC_CONSTANT
 #include <boost/math/special_functions/hypot.hpp>
+#include <boost/shared_ptr.hpp>
 
 // Classes and concepts to represent points in a space, with distance and move
 // operations (used for Gurson-Atun layout), plus other things like bounding
