42983e1e573c19aa7b306cf9118109a740bca8c7
Andreas Scherer <andreas_github@freenet.de>
2026-08-30 21:52:18 +0200

    Discard single use of 'Distance' altogether.

--- example/miles_span.cpp
+++ example/miles_span.cpp
@@ -105,8 +105,9 @@ int main(int argc, char* argv[])
         auto d = get(z_property< long >(), g);
         // Use the "w" property for parent
         auto p = get(w_property< Vertex* >(), g);
-        using Distance = property_map< Graph*, z_property< long > >::type;
-        total_length_visitor< Distance > length_vis(sp_length, d);
+        total_length_visitor<
+            property_map< Graph*, z_property< long > >::type
+            > length_vis(sp_length, d);
 
         prim_minimum_spanning_tree(g, p,
             distance_map(get(z_property< long >(), g))
